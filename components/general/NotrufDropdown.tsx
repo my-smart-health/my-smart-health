7d2befe1240b2bbba3f5e595229c5fd6ffd5c06e
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-19 19:58:08 +0300

    Refactor NotrufDropdown component for improved styling and layout consistency

--- components/general/NotrufDropdown.tsx
+++ components/general/NotrufDropdown.tsx
@@ -14,24 +14,24 @@ export default function NotrufButton() {
           height={55}
           className={`hover:scale-110 transition-transform ease-in-out shadow-lg shadow-[${SHADOW_COLOR}]`} />
       </summary>
-      <ul className={`menu dropdown-content mr-1 w-fit bg-white text-xl font-black z-1 shadow-lg shadow-[${SHADOW_COLOR}] border-6 border-[${BORDER_COLOR}]`}>
+      <ul className={`menu dropdown-content mr-1 w-64 p-0 bg-white text-xl font-black z-1 shadow-lg shadow-[${SHADOW_COLOR}] border-6 border-[${BORDER_COLOR}]`}>
         <li>
           <a
             href="tel:112"
-            className={`hover:underline cursor-pointer border-b-2 px-4 py-2 border-[${BORDER_COLOR}]`}
+            className={`hover:underline cursor-pointer border-b-2 px-5 py-2 border-[${BORDER_COLOR}]`}
           >
             Notruf<br />112
           </a>
         </li>
         <li><a
           href="tel:116117"
-          className={`hover:underline cursor-pointer border-b-2 px-4 py-2 border-[${BORDER_COLOR}]`}
+          className={`hover:underline cursor-pointer border-b-2 px-5 py-2 border-[${BORDER_COLOR}]`}
         >
           Bereitschaftsarzt<br />116117
         </a></li>
         <li><a
           href="tel:19222"
-          className="hover:underline cursor-pointer px-4 py-2"
+          className="hover:underline cursor-pointer px-5 py-2"
         >
           Krankentransport<br />19222
         </a></li>
@@ -39,37 +39,3 @@ export default function NotrufButton() {
     </details >
   )
 }
-{/* <button
-        draggable={false}
-        onClick={handleClick}
-        className="cursor-pointer">
-        <Image
-          draggable={false}
-          src="/icon1.png"
-          alt="Notruf"
-          width={60}
-          height={55}
-          className="hover:scale-110 transition-transform ease-in-out shadow-lg shadow-black/60" />
-      </button>
-      <div
-        className={`flex flex-col absolute top-26 right-1 border-5 text-xl font-extrabold bg-white border-[#2db9bc] shadow-xl shadow-[#2a2a2ad9] transition-all ease-in-out duration-1000`}
-      >
-        <a
-          href="tel:112"
-          className="hover:underline cursor-pointer border-b-2 px-6 py-2 border-[#2db9bc]"
-        >
-          Notruf<br />112
-        </a>
-        <a
-          href="tel:116117"
-          className="hover:underline cursor-pointer border-b-2 px-6 py-2 border-[#2db9bc]"
-        >
-          Bereitschaftsarzt<br />116117
-        </a>
-        <a
-          href="tel:19222"
-          className="hover:underline cursor-pointer border-b-2 px-6 py-2 border-[#2db9bc]"
-        >
-          Krankentransport<br />19222
-        </a>
-      </div> */}
