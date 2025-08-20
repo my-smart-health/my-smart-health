ae2c847ad2dafb88c23b400503e704e8187bde91
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 01:36:29 +0300

    fix: update layout structure for improved organization and styling consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 import NewsCarousel from "@/components/carousels/newsCarousel/NewsCarousel";
 import TopCarousel from "@/components/carousels/topCarousel/TopCarousel";
+import NewsSmartHealthMedizinButton from "@/components/news-smart-health-medizin-button/NewsSmartHealthMedizinButton";
+import TheHealthBarLink from "@/components/the-health-bar-link/TheHealthBarLink";
 import { defaultCarouselItems, defaultNewsItems } from "@/data/mockup-data";
 
 export default function Home() {
 
   return (
-    <div className="font-sans flex flex-col h-[100dvh] justify-items-center">
+    <div className="font-sans flex flex-col min-h-[100dvh] justify-items-center">
       <main className="flex flex-col items-center gap-8">
-        <div className="flex flex-col gap-4 max-w-md">
+        <div className="flex flex-col items-center gap-8 max-w-md">
           <TopCarousel props={defaultCarouselItems} />
           <NewsCarousel props={defaultNewsItems} />
+        </div>
+        <div className="flex flex-col gap-4 max-w-md">
+          <NewsSmartHealthMedizinButton name="News" icon="/icon2.png" goTo="/news" />
+          <NewsSmartHealthMedizinButton name="Smart Health" icon="/icon3.png" goTo="/news" />
+          <NewsSmartHealthMedizinButton name="Medizin & Pflege" icon="/icon4.png" goTo="/news" />
+          <TheHealthBarLink />
         </div>
       </main>
     </div>
