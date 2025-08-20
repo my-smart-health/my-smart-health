d6590080cb9c06f415bd123f6ff5ce10fe2daf30
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 01:36:13 +0300

    fix: refactor layout structure for improved organization and styling consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/general/Navbar";
+import Link from "next/link";
 
 
 const montserrat = Montserrat({
@@ -42,12 +43,20 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body
-        className={`${montserrat.variable} antialiased flex flex-col justify-center items-center border min-h-[100dvh] w-auto max-w-md lg:max-w-md mx-auto sm:px-6 lg:px-8 overscroll-none`}
+        className={`${montserrat.variable} antialiased flex flex-col justify-center items-center pt-4 min-h-[100dvh] w-auto lg:max-w-md mx-auto sm:px-6 lg:px-8 overscroll-none `}
       >
-        <Navbar />
-        {children}
-        <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-          &copy; {new Date().getFullYear()} My Smart Health
+        <div className="flex flex-col w-full h-full justify-between max-w-md lg:max-w-md">
+          <Navbar />
+          {children}
+        </div>
+        <footer className="flex flex-row w-full md:max-xl:w-full items-center justify-center py-2 text-white font-thin bg-[#2db9bc]">
+          <Link href="/impressum">IMPRESSUM</Link>
+          <br />
+          <span className="mx-1">|</span>
+          <Link href="/datenschutz">DATENSCHUTZ</Link>
+          <br />
+          <span className="mx-1">|</span>
+          <Link href="/kontakt">KONTAKT</Link>
         </footer>
       </body>
     </html>
