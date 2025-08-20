b5e4debbad6810392045e56324c508cc33f1680b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-20 16:20:17 +0300

    Add default news items to mockup data for enhanced content display

--- data/mockup-data.ts
+++ data/mockup-data.ts
@@ -42,3 +42,47 @@ export const defaultCarouselItems = [
     name: 'Name',
   },
 ];
+export const defaultNewsItems = [
+  {
+    id: '1',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
+    info: 'Info',
+  },
+  {
+    id: '2',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp',
+    info: 'Info',
+  },
+  {
+    id: '3',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp',
+    info: 'Info',
+  },
+  {
+    id: '4',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
+    info: 'Info',
+  },
+  {
+    id: '5',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp',
+    info: 'Info',
+  },
+  {
+    id: '6',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
+    info: 'Info',
+  },
+  {
+    id: '7',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
+    info: 'Info',
+  },
+];
