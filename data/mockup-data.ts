61a0266471be67db9bf4c406a7ffb8f7e3ca01eb
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 01:40:16 +0300

    fix: update image source for the last carousel item in mockup data

--- data/mockup-data.ts
+++ data/mockup-data.ts
@@ -38,7 +38,7 @@ export const defaultCarouselItems = [
   {
     id: '7',
     imageSrc:
-      'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
+      'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
     name: 'Name',
   },
 ];
