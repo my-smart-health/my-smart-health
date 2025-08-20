c0a02d063453c4e360ea2a42f3fee39436679a09
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-20 19:34:10 +0300

    fix: update layout to use h-[100dvh] for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,10 @@ import { defaultCarouselItems, defaultNewsItems } from "@/data/mockup-data";
 export default function Home() {
 
   return (
-    <div className="font-sans flex flex-col h-screen justify-items-center gap-16 sm:p-20 ">
-      <main className="flex flex-col items-center gap-8 min-h-screen">
-        <div className="max-w-[85vw] sm:max-w-[80vw] md:max-w-[60vw] lg:max-w-[36vw]">
+    <div className="font-sans flex flex-col h-[100dvh] justify-items-center">
+      <main className="flex flex-col items-center gap-8">
+        <div className="flex flex-col gap-4 sm:max-w-[80vw] md:max-w-[60vw] lg:max-w-[30vw] max-w-[90vw]">
           <TopCarousel props={defaultCarouselItems} />
-        </div>
-        <div className="max-w-[85vw] sm:max-w-[80vw] md:max-w-[60vw] lg:max-w-[36vw]">
           <NewsCarousel props={defaultNewsItems} />
         </div>
       </main>
