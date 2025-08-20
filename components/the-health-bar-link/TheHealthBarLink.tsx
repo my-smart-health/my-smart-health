3a394547cfd2e17578ad9086b387b3815ce770dd
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 01:37:12 +0300

    feat: add TheHealthBarLink component for navigation to health bar

---
+++ components/the-health-bar-link/TheHealthBarLink.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function TheHealthBarLink() {
+  return (
+    <Link href="/health-bar" className="max-w-sm">
+      <Image loading="lazy" placeholder="empty" src="/healthbar.png" alt="Health Bar Icon" width={300} height={130} className="max-w-sm h-[130px]" />
+    </Link>
+  );
+}
