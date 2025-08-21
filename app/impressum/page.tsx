3de4cb6a8cbc595e8f26d1d9e504701e87e9389d
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 19:52:05 +0300

    feat: add ImpressumPage component for legal information display

---
+++ app/impressum/page.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+export default function ImpressumPage() {
+  return (
+    <div className="mb-auto flex flex-col align-baseline justify-baseline ml-4 w-full max-w-[90%]">
+      <h1>Impressum</h1>
+      <p>Diese Webseite wird betrieben von:</p>
+      <br />
+      <p>
+        Future Health GmbH
+        <br />
+        Wildenbruchstr. 13
+        <br />
+        40545 Düsseldorf
+        <br />
+        Deutschland
+        <br />
+        Web: <Link href="https://www.tlhow.com" title="Website" className="link not-visited:text-blue-600 visited:text-purple-600">www.tlhow.com</Link>
+        <br />
+        Mail: <Link href="mailto:f.jeute@spitzenmedizin.com" title="Email" className="link not-visited:text-blue-600 visited:text-purple-600">f.jeute@spitzenmedizin.com</Link>
+      </p>
+      <br />
+      <p>
+        Sitz der Gesellschaft: Düsseldorf, Deutschland
+        <br />
+        Vertretungsberechtigter Geschäftsführer: Dr. Ferdinand Jeute
+        <br />
+        HRB Nummer 72666 im Handelsregister des Amtsgericht Düsseldorf, Deutschland
+        <br />
+        <br />
+        Umsatzsteuer-Identifikationsnummer DE295913675
+      </p>
+    </div>
+  );
+}
