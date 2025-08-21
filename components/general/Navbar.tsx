49a080275c7f773f57341d0a664314bcf957d763
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 18:12:28 +0300

    fix: update Navbar styles for improved layout and consistency

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
   return (
     <nav
       draggable={false}
-      className="flex flex-col justify-evenly mb-4 w-full max-w-xs sm:max-w-xs md:max-w-sm"
+      className="flex flex-col mx-auto mb-4 w-full max-w-[90%] z-10"
     >
       <div className="flex flex-row gap-1 items-center justify-evenly text-start text-nowrap">
         <Link
@@ -17,20 +17,15 @@ export default function Navbar() {
           className="flex flex-col items-start sm:items-center"
         >
           <div className="flex flex-row items-center justify-center text-xl font-bold uppercase bg-white">
-            My Smart
             <Image
               priority
               draggable={false}
-              src="/logo.png"
+              src="/Logo.jpg"
               alt="My Smart Health"
-              width={32}
-              height={32}
-              className="w-10 h-auto mx-1"
+              width={350}
+              height={112}
+              className="w-96 h-auto mx-auto"
             />
-            Health
-          </div>
-          <div className="text-xs font-medium text-neutral-600 uppercase bg-white">
-            Wir machen Düsseldorf gesünder
           </div>
         </Link>
         <NotrufDropdown />
