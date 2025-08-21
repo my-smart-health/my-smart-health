779caa7ead4e693ed2a3b8035924098418897215
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 18:12:18 +0300

    fix: adjust layout and spacing in Home component for improved consistency

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
 
   return (
     <div className="font-sans flex flex-col min-h-[100dvh] justify-items-center">
-      <main className="flex flex-col items-center gap-8">
-        <div className="flex flex-col items-center gap-8 max-w-md">
+      <main className="flex flex-col items-center gap-8 max-w-md">
+        <div className="flex flex-col items-center gap-8">
           <TopCarousel props={defaultCarouselItems} />
           <NewsCarousel props={defaultNewsItems} />
         </div>
-        <div className="flex flex-col gap-4 max-w-md">
+        <div className="flex flex-col gap-3 w-full mx-auto max-w-[90%] mb-2">
           <NewsSmartHealthMedizinButton name="News" icon="/icon2.png" goTo="/news" />
           <NewsSmartHealthMedizinButton name="Smart Health" icon="/icon3.png" goTo="/news" />
           <NewsSmartHealthMedizinButton name="Medizin & Pflege" icon="/icon4.png" goTo="/news" />
