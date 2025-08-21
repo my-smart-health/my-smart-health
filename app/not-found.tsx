b01be5d52abe9a1deb4fd394ac21d5986d43b2dd
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 20:22:32 +0300

    fix: update NotFoundPage text to use HTML entity for apostrophe

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center m-auto">
       <h1 className="text-4xl font-bold">404 - Not Found</h1>
-      <p className="mt-4">We're sorry, but the page you are looking for does not exist.</p>
+      <p className="mt-4">We&apos;re sorry, but the page you are looking for does not exist.</p>
       <Link className="mt-4 px-4 py-2 bg-primary text-white rounded" href="/">Go back</Link>
     </div>
   );
