0185ce2122ee4bb6cfcc6b0d8ab5e9cbf2e15cde
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 19:50:28 +0300

    feat: add NotFoundPage component for handling 404 errors

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center m-auto">
+      <h1 className="text-4xl font-bold">404 - Not Found</h1>
+      <p className="mt-4">We're sorry, but the page you are looking for does not exist.</p>
+      <Link className="mt-4 px-4 py-2 bg-[#2db9bc] text-white rounded" href="/">Go back</Link>
+    </div>
+  );
+}
