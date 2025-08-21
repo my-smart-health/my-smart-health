e4a324297ecb7b83daa6f173036d32b31ca45491
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 18:12:33 +0300

    fix: remove max-width from NewsSmartHealthMedizinButton for improved layout flexibility

--- components/news-smart-health-medizin-button/NewsSmartHealthMedizinButton.tsx
+++ components/news-smart-health-medizin-button/NewsSmartHealthMedizinButton.tsx
@@ -9,7 +9,7 @@ type NewsSmartHealthMedizinButtonProps = {
 
 export default function NewsSmartHealthMedizinButton({ name, icon, goTo }: NewsSmartHealthMedizinButtonProps) {
   return (
-    <Link href={goTo} className="flex flex-row h-15 rounded-2xl border shadow-xl max-w-sm">
+    <Link href={goTo} className="flex flex-row h-15 rounded-2xl border shadow-xl">
       <div className="w-14 rounded-tl-2xl rounded-bl-2xl flex items-center justify-center bg-[#2db9bc] text-white">
         <Image priority src={icon} width={26} height={26} alt={`${name} icon`} className="w-7 h-7 bg-[#2db9bc] text-white" />
       </div>
