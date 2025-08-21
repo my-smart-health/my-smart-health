1d02991acc9c1bb4a5e4a713ab77106175de7b40
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 18:12:44 +0300

    fix: update layout and image dimensions in TheHealthBarLink component for improved responsiveness

--- components/the-health-bar-link/TheHealthBarLink.tsx
+++ components/the-health-bar-link/TheHealthBarLink.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function TheHealthBarLink() {
   return (
-    <Link draggable={false} href="/health-bar" className="max-w-sm">
-      <Image loading="lazy" placeholder="empty" src="/healthbar.png" alt="Health Bar Icon" width={300} height={130} className="max-w-sm h-[130px]" />
+    <Link draggable={false} href="/health-bar" className="flex justify-center w-full mx-auto max-w-[90%]">
+      <Image loading="lazy" placeholder="empty" src="/healthbar.png" alt="Health Bar Icon" width={410} height={130} className="sm:max-w-sm h-[130px]" />
     </Link>
   );
 }
