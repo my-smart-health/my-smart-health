a16ca4f30836e2cfa1764b517f048bfad9df0b6a
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-21 23:10:29 +0300

    fix: update href in TheHealthBarLink component to correct navigation path

--- components/the-health-bar-link/TheHealthBarLink.tsx
+++ components/the-health-bar-link/TheHealthBarLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function TheHealthBarLink() {
   return (
-    <Link draggable={false} href="/health-bar" className="flex justify-center w-full mx-auto max-w-[90%]">
+    <Link draggable={false} href="/the-health-bar" className="flex justify-center w-full mx-auto max-w-[90%]">
       <Image loading="lazy" placeholder="empty" src="/healthbar.png" alt="Health Bar Icon" width={410} height={130} className="sm:max-w-sm h-[130px]" />
     </Link>
   );
