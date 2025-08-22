2642261468ab51574bcd5dba7250678cb70b131b
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-22 17:58:38 +0300

    fix: add title attribute and adjust Image styles in TheHealthBarLink component for improved accessibility and layout

--- components/the-health-bar-link/TheHealthBarLink.tsx
+++ components/the-health-bar-link/TheHealthBarLink.tsx
@@ -3,8 +3,22 @@ import Link from "next/link";
 
 export default function TheHealthBarLink() {
   return (
-    <Link draggable={false} href="/the-health-bar" className="flex justify-center w-full mx-auto max-w-[90%]">
-      <Image loading="lazy" placeholder="empty" src="/healthbar.png" alt="Health Bar Icon" width={410} height={130} className="sm:max-w-sm h-[130px]" />
+    <Link
+      draggable={false}
+      href="/the-health-bar"
+      className="flex justify-center w-full mx-auto max-w-[90%]"
+      title="The Health Bar"
+    >
+      <Image
+        loading="lazy"
+        placeholder="empty"
+        src="/healthbar.png"
+        alt="Health Bar Icon"
+        width={410}
+        height={130}
+        className="sm:max-w-sm w-auto h-[130px]"
+        style={{ width: "auto", height: "auto" }}
+      />
     </Link>
   );
 }
