bd943de6001a4eb0cf8a5fa59dec990213aecb84
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-22 17:16:57 +0300

    fix: update carousel and news item IDs for consistency; add health bar items

--- data/mockup-data.ts
+++ data/mockup-data.ts
@@ -1,42 +1,42 @@
 export const defaultCarouselItems = [
   {
-    id: '1',
+    id: 'carousel-1',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
     name: 'Name',
   },
   {
-    id: '2',
+    id: 'carousel-2',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp',
     name: 'Name',
   },
   {
-    id: '3',
+    id: 'carousel-3',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp',
     name: 'Name',
   },
   {
-    id: '4',
+    id: 'carousel-4',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
     name: 'Name',
   },
   {
-    id: '5',
+    id: 'carousel-5',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp',
     name: 'Name',
   },
   {
-    id: '6',
+    id: 'carousel-6',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
     name: 'Name',
   },
   {
-    id: '7',
+    id: 'carousel-7',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
     name: 'Name',
@@ -44,45 +44,89 @@ export const defaultCarouselItems = [
 ];
 export const defaultNewsItems = [
   {
-    id: '1',
+    id: 'news-1',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
     info: 'Info',
   },
   {
-    id: '2',
+    id: 'news-2',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp',
     info: 'Info',
   },
   {
-    id: '3',
+    id: 'news-3',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp',
     info: 'Info',
   },
   {
-    id: '4',
+    id: 'news-4',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
     info: 'Info',
   },
   {
-    id: '5',
+    id: 'news-5',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp',
     info: 'Info',
   },
   {
-    id: '6',
+    id: 'news-6',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
     info: 'Info',
   },
   {
-    id: '7',
+    id: 'news-7',
     imageSrc:
       'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
     info: 'Info',
+  },
+];
+export const defaultHealthBarItems = [
+  {
+    id: 'healthbar-1',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp',
+    link: '/the-health-bar',
+  },
+  {
+    id: 'healthbar-2',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp',
+    link: '/the-health-bar',
+  },
+  {
+    id: 'healthbar-3',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp',
+    link: '/the-health-bar',
+  },
+  {
+    id: 'healthbar-4',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
+    link: '/the-health-bar',
+  },
+  {
+    id: 'healthbar-5',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp',
+    link: '/the-health-bar',
+  },
+  {
+    id: 'healthbar-6',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp',
+    link: '/the-health-bar',
+  },
+  {
+    id: 'healthbar-7',
+    imageSrc:
+      'https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp',
+    link: '/the-health-bar',
   },
 ];
