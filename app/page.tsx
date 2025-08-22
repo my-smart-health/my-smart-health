767877ece23fc751f6175617314b4f2e50707b16
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-22 17:16:49 +0300

    fix: simplify layout structure in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,9 @@ export default function Home() {
   return (
     <div className="font-sans flex flex-col min-h-[100dvh] justify-items-center">
       <main className="flex flex-col items-center gap-8 max-w-md">
-        <div className="flex flex-col items-center gap-8">
-          <TopCarousel props={defaultCarouselItems} />
-          <NewsCarousel props={defaultNewsItems} />
-        </div>
-        <div className="flex flex-col gap-3 w-full mx-auto max-w-[90%] mb-2">
+        <TopCarousel props={defaultCarouselItems} />
+        <NewsCarousel props={defaultNewsItems} />
+        <div className="flex flex-col gap-3 w-full mx-auto max-w-[90%]">
           <NewsSmartHealthMedizinButton name="News" icon="/icon2.png" goTo="/news" />
           <NewsSmartHealthMedizinButton name="Smart Health" icon="/icon3.png" goTo="/smart-health" />
           <NewsSmartHealthMedizinButton name="Medizin & Pflege" icon="/icon4.png" goTo="/medizin-und-pflege" />
