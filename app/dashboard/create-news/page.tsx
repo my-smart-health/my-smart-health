35da2327385f5536a0cde86f3363c96cccd09850
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-28 03:43:01 +0300

    fix: implement session authentication and redirect to login in CreateNewsPage

--- app/dashboard/create-news/page.tsx
+++ app/dashboard/create-news/page.tsx
@@ -1,10 +1,17 @@
+import { auth } from "@/auth";
 import CreateNewsForm from "./CreateNewsForm";
+import { redirect } from "next/navigation";
 
 export default async function CreateNewsPage() {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <main className="flex flex-col gap-4 h-full min-h-[72dvh] max-w-[90%]">
-      <h1>Create News</h1>
-      <CreateNewsForm />
+      <h1 className="text-4xl font-bold text-primary text-center">Create News</h1>
+      <CreateNewsForm session={session} />
     </main>
   );
 }
