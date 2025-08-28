7e70b2a0136760e504ee29e386c8d6ad88ea4f0f
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-28 03:28:35 +0300

    fix: add additional remote pattern for image loading in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://img.daisyui.com/images/stock/**')],
+    remotePatterns: [
+      new URL('https://img.daisyui.com/images/stock/**'),
+      new URL('https://nlj4slf2dv4x65qm.public.blob.vercel-storage.com/**'),
+    ],
   },
 };
 
