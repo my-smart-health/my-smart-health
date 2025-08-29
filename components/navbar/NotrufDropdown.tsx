c758e4dd39e54917d86bafce12200487c0bef6b3
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-29 20:21:32 +0300

    fix: refactor NotrufButton component to use div structure for improved accessibility and styling

--- components/navbar/NotrufDropdown.tsx
+++ components/navbar/NotrufDropdown.tsx
@@ -3,39 +3,43 @@ import Image from "next/image";
 export default function NotrufButton() {
 
   return (
-    <details className="dropdown dropdown-end border-0">
-      <summary className="btn btn-square m-1 border-0 h-16 w-16 bg-white hover:bg-white shadow-none">
-        <Image
-          priority
-          draggable={false}
-          src="/icon1.png"
-          alt="Notruf"
-          width={60}
-          height={60}
-          className={`w-[60px] h-[60px] hover:scale-110 transition-transform ease-in-out shadow-lg shadow-[#2a2a2ad9]`} />
-      </summary>
-      <ul className={`menu dropdown-content mr-1 w-64 p-0 bg-white text-xl font-black z-1 shadow-lg shadow-[#2a2a2ad9] border-6 border-primary`}>
-        <li>
-          <a
-            href="tel:112"
+    <div className="navbar-end">
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-square w-full">
+          <Image
+            priority
+            draggable={false}
+            src="/icon1.png"
+            alt="Notruf"
+            width={60}
+            height={60}
+            className={`w-[60px] h-[60px] aspect-square hover:scale-110 transition-transform ease-in-out shadow-lg shadow-[#2a2a2ad9]`} />
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-md dropdown-center dropdown-content bg-base-100 rounded-box z-1 mt-4 w-52 p-2 shadow border-3 border-primary">
+          <li>
+            <a
+              href="tel:112"
+              className={`hover:underline cursor-pointer border-b-2 px-5 py-2 border-primary`}
+            >
+              Notruf<br />112
+            </a>
+          </li>
+          <li><a
+            href="tel:116117"
             className={`hover:underline cursor-pointer border-b-2 px-5 py-2 border-primary`}
           >
-            Notruf<br />112
-          </a>
-        </li>
-        <li><a
-          href="tel:116117"
-          className={`hover:underline cursor-pointer border-b-2 px-5 py-2 border-primary`}
-        >
-          Bereitschaftsarzt<br />116117
-        </a></li>
-        <li><a
-          href="tel:19222"
-          className="hover:underline cursor-pointer px-5 py-2"
-        >
-          Krankentransport<br />19222
-        </a></li>
-      </ul>
-    </details >
-  )
+            Bereitschaftsarzt<br />116117
+          </a></li>
+          <li><a
+            href="tel:19222"
+            className="hover:underline cursor-pointer px-5 py-2"
+          >
+            Krankentransport<br />19222
+          </a></li>
+        </ul>
+      </div>
+    </div>
+  );
 }
