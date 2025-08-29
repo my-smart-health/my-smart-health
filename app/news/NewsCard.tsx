67865415556542040814400d8ffe30604bf99978
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-29 18:47:35 +0300

    feat: implement NewsCard component and integrate it into NewsPage for displaying news articles

---
+++ app/news/NewsCard.tsx
@@ -0,0 +1,98 @@
+'use client'
+import { NewsCardType } from "@/utils/types";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Mousewheel, Scrollbar } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import 'swiper/css/mousewheel';
+import 'swiper/css/effect-fade';
+
+
+import { useEffect, useState } from "react";
+import GoToButton from "@/components/buttons/go-to/GoToButton";
+
+export default function NewsCard({ newsData }: { newsData: NewsCardType[] }) {
+  return (
+    <>
+      {newsData && newsData.map((news) => {
+        const { id, title, content, createdAt, photos, author } = news;
+        const [createdDate, setCreatedDate] = useState('');
+
+        useEffect(() => {
+          setCreatedDate(new Date(createdAt).toLocaleString('de-DE', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+          }));
+        }, [createdAt]);
+
+        return (
+          <div
+            key={id}
+            className="m-auto min-h-full border max-w-[90%] rounded-lg shadow-2xl"
+          >
+            <div className="card bg-secondary/20 w-96 shadow-sm max-w-[100%]">
+              <div className="badge badge-accent rounded-bl-none rounded-tr-none p-4">{createdDate}</div>
+              <h2 className="card-title card-border flex-col m-4 justify-center">
+                {title}
+              </h2>
+              <div className="card-body">
+                <figure>
+                  <div className="max-w-xs md:max-w-sm">
+                    <Swiper
+                      modules={[Scrollbar, Mousewheel, Autoplay, EffectFade]}
+                      spaceBetween={0}
+                      lazyPreloadPrevNext={3}
+                      slidesPerView={1}
+                      effect="fade"
+                      grabCursor={true}
+                      fadeEffect={{
+                        crossFade: true
+                      }}
+                      mousewheel={true}
+                      autoplay={{ delay: 3000, disableOnInteraction: false }}
+                      speed={1500}
+                      scrollbar={{ draggable: true }}
+                    >
+                      {photos.map((item, idx) => (
+                        <SwiperSlide key={idx}>
+                          <div className="flex flex-col justify-center items-center rounded-box cursor-pointer max-w-full">
+                            <Image
+                              loading="lazy"
+                              placeholder="empty"
+                              width={450}
+                              height={150}
+                              src={item}
+                              alt={item}
+                              className="aspect-video w-full object-center object-cover"
+                            />
+                          </div>
+                          <br />
+                        </SwiperSlide>
+                      ))}
+                    </Swiper>
+                  </div>
+                </figure>
+                <p className="text-base line-clamp-3">{content}</p>
+                <div className="card-actions justify-end">
+                  {
+                    author?.fieldOfExpertise.map((expertise, index) => (
+                      <div key={index} className="badge badge-outline">{expertise}</div>
+                    ))}
+                </div>
+                <div className="card-actions justify-center">
+                  <GoToButton name={"View more"} src={`/news/${id}`} className="btn btn-wide bg-primary rounded-xl text-secondary" />
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,16 +1,45 @@
 import GoBack from "@/components/buttons/go-back/GoBack";
 import NewsSmartHealthMedizinButton from "@/components/news-smart-health-medizin-button/NewsSmartHealthMedizinButton";
+import prisma from "@/lib/db";
+import NewsCard from "./NewsCard";
+import { NewsCardType } from "@/utils/types";
 
-export default function NewsPage() {
+async function getData() {
+  const prismaResult = await prisma.posts.findMany({
+    take: 20,
+    orderBy: {
+      createdAt: "desc",
+    },
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+      photos: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          fieldOfExpertise: true,
+        }
+      }
+    }
+  })
+
+  return prismaResult;
+}
+
+export default async function NewsPage() {
+  const newsData = await getData() as NewsCardType[];
+
   return (
     <div className="flex flex-col gap-3 w-full mb-auto max-w-[90%]">
       <NewsSmartHealthMedizinButton name="News" icon="/icon2.png" goTo="/news" active />
       <div className="flex justify-end">
         <GoBack />
       </div>
-      <div className="m-auto min-h-full border">
-        <p className="p-4">News content goes here</p>
-      </div>
+      {newsData && <NewsCard newsData={newsData} />}
       <NewsSmartHealthMedizinButton name="Smart Health" icon="/icon3.png" goTo="/smart-health" />
       <NewsSmartHealthMedizinButton name="Medizin & Pflege" icon="/icon4.png" goTo="/medizin-und-pflege" />
     </div>
