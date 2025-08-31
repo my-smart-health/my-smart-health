88fd835b196b048a551eb9c3a4f78a52c8993757
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-08-31 18:03:52 +0300

    feat: implement POST endpoint for uploading user profile pictures

---
+++ app/api/upload-profile-picture/route.tsx
@@ -0,0 +1,32 @@
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const filename = searchParams.get('filename');
+    const userid = searchParams.get('userid');
+
+    if (!userid) {
+      return NextResponse.json({ error: 'User ID is required' }, { status: 400 });
+    }
+
+    if (!filename) {
+      return NextResponse.json({ error: 'Filename is required' }, { status: 400 });
+    }
+    if (!request.body) {
+      return NextResponse.json({ error: 'File is required' }, { status: 400 });
+    }
+
+    const blob = await put(`${userid}/profile-pictures/${filename}`, request.body, {
+      access: 'public',
+      allowOverwrite: true
+    });
+
+    return NextResponse.json(blob, { status: 200 });
+  } catch (error) {
+    process.env.NODE_ENV === 'development' &&
+      console.error('Error uploading picture:', error);
+    return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+  }
+}
