6de7d4af6c0481dfb705d600cdb1747368d2579d
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-01 13:24:53 +0300

    feat: refactor image remotePatterns in next.config.ts for improved clarity and structure

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,18 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL('https://img.daisyui.com/images/stock/**'),
-      new URL('https://nlj4slf2dv4x65qm.public.blob.vercel-storage.com/**'),
+      {
+        protocol: 'https',
+        hostname: 'img.daisyui.com',
+        pathname: '/images/stock/**',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nlj4slf2dv4x65qm.public.blob.vercel-storage.com',
+        pathname: '/**',
+        port: '',
+      },
     ],
   },
 };
