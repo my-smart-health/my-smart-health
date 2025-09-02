ec97bfecd94227b344f3e0ad44df658e9ee6b6cd
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-02 03:53:48 +0300

    feat: replace Swiper with FadeCarousel for image display in NewsCard component

--- app/news/NewsCard.tsx
+++ app/news/NewsCard.tsx
@@ -1,15 +1,10 @@
 'use client'
+
 import { NewsCardType } from "@/utils/types";
-import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, EffectFade, Mousewheel, Scrollbar } from "swiper/modules";
-import 'swiper/css';
-import 'swiper/css/scrollbar';
-import 'swiper/css/autoplay';
-import 'swiper/css/mousewheel';
-import 'swiper/css/effect-fade';
 
 import GoToButton from "@/components/buttons/go-to/GoToButton";
+import FadeCarousel from "@/components/carousels/fade-carousel/FadeCarousel";
+import { Suspense } from "react";
 
 export default function NewsCard({ newsData }: { newsData: NewsCardType[] }) {
   return (
@@ -34,43 +29,10 @@ export default function NewsCard({ newsData }: { newsData: NewsCardType[] }) {
               <h2 className="card-title card-border flex-col m-4 justify-center">
                 {title}
               </h2>
-              <div className="card-body">
-                <figure>
-                  <div className="max-w-xs md:max-w-sm">
-                    <Swiper
-                      modules={[Scrollbar, Mousewheel, Autoplay, EffectFade]}
-                      spaceBetween={0}
-                      lazyPreloadPrevNext={3}
-                      slidesPerView={1}
-                      effect="fade"
-                      grabCursor={true}
-                      fadeEffect={{
-                        crossFade: true
-                      }}
-                      mousewheel={true}
-                      autoplay={{ delay: 3000, disableOnInteraction: false }}
-                      speed={1500}
-                      scrollbar={{ draggable: true }}
-                    >
-                      {photos.map((item, idx) => (
-                        <SwiperSlide key={idx}>
-                          <div className="flex flex-col justify-center items-center rounded-box cursor-pointer max-w-full">
-                            <Image
-                              loading="lazy"
-                              placeholder="empty"
-                              width={450}
-                              height={150}
-                              src={item}
-                              alt={item}
-                              className="aspect-video w-full object-center object-cover"
-                            />
-                          </div>
-                          <br />
-                        </SwiperSlide>
-                      ))}
-                    </Swiper>
-                  </div>
-                </figure>
+              <div className="card-body min-h-[352px]">
+                <Suspense fallback={<div className="text-center skeleton min-h-[352px]">Loading...</div>}>
+                  <FadeCarousel photos={photos} />
+                </Suspense>
                 <p className="text-base line-clamp-3 indent-6">{content}</p>
                 <div className="card-actions justify-end">
                   {
