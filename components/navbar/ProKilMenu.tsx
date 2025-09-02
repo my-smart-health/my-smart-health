7fd33262c5e2db36c1c712d75e31352abe355dd2
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-02 03:54:26 +0300

    feat: add ProKilMenu component for user session management and navigation

---
+++ components/navbar/ProKilMenu.tsx
@@ -0,0 +1,33 @@
+import LogOut from "../buttons/log-out/LogOut";
+import { Settings } from "lucide-react";
+import { auth } from "@/auth";
+import Link from "next/link";
+
+export default async function ProKilMenu() {
+  const session = await auth();
+
+  return (
+    <>
+      {session &&
+        <div className="flex flex-row justify-evenly align-baseline h-full max-h-fit w-full max-w-[100%] gap-2 bg-primary text-white p-2 mb-4">
+          <div className="flex text-wrap m-auto">Welcome, {session.user?.email}</div>
+
+          <div className="border border-white h-full w-0 my-4 text-transparent">.</div>
+
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-circle m-2 border-2 border-white bg-primary"><Settings className="text-white" /></div>
+            <ul tabIndex={0} className="dropdown-content menu bg-primary border rounded-box z-1 w-52 p-2 shadow-sm transition-opacity ease-in-out">
+              <li><Link href="/" className="flex gap-1">Home</Link></li>
+              <li><Link href="/dashboard" className="flex gap-1">Dashboard</Link></li>
+              <li><Link href="/dashboard/create-news" className="flex gap-1">Create News</Link></li>
+              <li className="tab-disabled"></li>
+              <li><Link href="/dashboard/edit-profile" className="flex gap-1">Edit profile</Link></li>
+              <li className="tab-disabled"></li>
+              <li className="my-2"><LogOut /></li>
+            </ul>
+          </div>
+        </div>
+      }
+    </>
+  );
+}
