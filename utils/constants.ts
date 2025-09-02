e2575ec287aa9e0195780804d6da73e6300a9cca
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-02 03:52:39 +0300

    feat: add constants for maximum files per news and user

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,3 +6,6 @@ export const NOTRUF_NUMBERS = {
 
 export const BORDER_COLOR = '#2db9bc';
 export const SHADOW_COLOR = '#2a2a2ad9';
+
+export const MAX_FILES_PER_NEWS = 5;
+export const MAX_FILES_PER_USER = 10;
