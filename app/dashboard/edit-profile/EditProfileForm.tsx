6163911e2ca045244f230b7cd8b8f53ee1473798
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-02 03:53:34 +0300

    feat: enhance profile editing with schedule management and multiple image uploads

--- app/dashboard/edit-profile/EditProfileForm.tsx
+++ app/dashboard/edit-profile/EditProfileForm.tsx
@@ -3,30 +3,32 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-import { useRef, useState } from "react";
+import { Suspense, useRef, useState } from "react";
 import { PutBlobResult } from "@vercel/blob";
 
 import GoToButton from "@/components/buttons/go-to/GoToButton";
 
-import { Social } from "@/utils/types";
+import { Schedule, Social } from "@/utils/types";
 import { parseSocials, serializeSocials } from "@/utils/common";
 
 import Xlogo from '@/public/x-logo-black.png';
 import TikTokLogo from '@/public/tik-tok-logo.png';
-import { AtSign, Facebook, Globe, Instagram, Linkedin, Phone, Youtube } from "lucide-react";
-
+import { AtSign, Facebook, Globe, Instagram, Linkedin, Phone, Youtube, ArrowUpRight } from "lucide-react";
+import { MAX_FILES_PER_USER } from "@/utils/constants";
+import FadeCarousel from "@/components/carousels/fade-carousel/FadeCarousel";
 
 type User = {
-  name: string | null;
-  address: string | null;
-  bio: string | null;
-  phone: string[];
-  website: string | null;
-  displayEmail: string | null;
-  image: string | null;
-  fieldOfExpertise: string[];
-  socials: string[];
   id: string;
+  phone: string[];
+  socials: string[];
+  bio: string | null;
+  name: string | null;
+  profileImages: string[] | null;
+  address: string | null;
+  website: string | null;
+  fieldOfExpertise: string[];
+  displayEmail: string | null;
+  schedule: Schedule[] | null;
 }
 
 export default function EditProfileForm({ user }: { user: User }) {
@@ -38,6 +40,9 @@ export default function EditProfileForm({ user }: { user: User }) {
   const [fieldOfExpertise, setFieldOfExpertise] = useState<string[]>(user.fieldOfExpertise || []);
   const [phoneNumbers, setPhoneNumbers] = useState<string[]>(user.phone || []);
   const [socials, setSocials] = useState<Social[]>(parseSocials(user.socials || []));
+  const [schedule, setSchedule] = useState<Schedule[]>(user.schedule || []);
+
+  const [error, setError] = useState<string | null>(null);
 
   const redirect = useRouter();
   const blobResult: string[] = [];
@@ -54,9 +59,27 @@ export default function EditProfileForm({ user }: { user: User }) {
     Instagram: <Instagram className="inline-block mr-1" size={20} />,
   };
 
+  const toggleScheduleDay = (id: string, day: keyof Schedule['day']) => {
+    setSchedule(schedule.map(schedule => schedule.id === id ? { ...schedule, day: { ...schedule.day, [day]: !schedule.day[day] } } : schedule));
+  };
+
+  const setScheduleTime = (id: string, openClose: keyof Schedule, value: string) => {
+    setSchedule(schedule.map(schedule => schedule.id === id ? { ...schedule, [openClose]: value.toString() } : schedule));
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    let files: File[];
+
+    if (inputFileRef.current && inputFileRef.current.files) {
+      files = Array.from(inputFileRef.current.files);
+    } else {
+      files = [];
+    }
+
     const formData = new FormData(e.currentTarget);
+
     const data = {
       name: formData.get('name') as string,
       bio: formData.get('bio') as string,
@@ -65,35 +88,38 @@ export default function EditProfileForm({ user }: { user: User }) {
       displayEmail: formData.get('displayEmail') as string,
       phone: phoneNumbers,
       website: formData.get('website') as string,
-      image: userData.image,
+      profileImages: userData.profileImages,
       socials: serializeSocials(socials),
+      schedule: userData.schedule,
     };
-    if (blobResult.length > 0) data.image = blobResult[0];
 
-    if (formData.get('image') && inputFileRef.current?.files?.[0]) {
-      try {
-        const file = formData.get('image') as File;
-        const response = await fetch(
-          `/api/upload-profile-picture/?userid=${userData.id}&filename=${file.name}`,
-          {
-            method: 'POST',
-            body: file,
-          }
-        );
-        if (!response.ok) throw new Error('Failed to upload image');
-        const result = await response.json() as PutBlobResult;
-        blobResult.push(result.url);
-      } catch (error) {
-        process.env.NODE_ENV === 'development' && console.error('Error uploading image:', error);
-        return;
+    if (blobResult.length > 0) data.profileImages = blobResult;
+
+    if (inputFileRef.current && inputFileRef.current.files) {
+      for (const file of files) {
+        try {
+          const response = await fetch(
+            `/api/upload-profile-picture/?userid=${userData.id}&filename=${file.name}`,
+            {
+              method: 'POST',
+              body: file,
+            }
+          );
+          if (!response.ok) throw new Error('Failed to upload image');
+          const result = await response.json() as PutBlobResult;
+          blobResult.push(result.url);
+        } catch (error) {
+          process.env.NODE_ENV === 'development' && console.error('Error uploading image:', error);
+          return;
+        }
       }
     }
+
     try {
-      if (userData.image) data.image = userData.image;
-      if (blobResult.length > 0) data.image = blobResult[0];
-
+      if (blobResult.length > 0) data.profileImages = blobResult;
       const payload = { ...data, socials: serializeSocials(socials) };
-
+      if (schedule.length > 0) payload.schedule = schedule;
+      console.log('Payload to be sent:', payload);
       const res = await fetch('/api/update-profile', {
         method: 'PUT',
         body: JSON.stringify({ id: userData.id, data: payload }),
@@ -102,6 +128,7 @@ export default function EditProfileForm({ user }: { user: User }) {
 
       if (!res.ok) throw new Error('Failed to update profile');
       const result = await res.json();
+      console.log('Profile updated successfully:', result);
       setUserData(result.data);
 
       // redirect.push('/dashboard');
@@ -113,22 +140,17 @@ export default function EditProfileForm({ user }: { user: User }) {
 
   return (
     <>
+      {error && <p className="text-red-500 p-2">{error}</p>}
       <form
         onSubmit={handleSubmit}
         className="w-full max-w-3xl rounded-2xl shadow-xl p-8 flex flex-col border-1 border-primary gap-8"
       >
-        {userData.image ? (
-          <Image
-            loading="lazy"
-            src={userData.image}
-            alt={userData.name || "Profile image"}
-            width={220}
-            height={220}
-            className="aspect-square w-full border-4 border-primary shadow-lg" />
+        {userData.profileImages ? (
+          <Suspense fallback={<div className="text-center skeleton min-h-[352px]">Loading...</div>}>
+            <FadeCarousel photos={userData.profileImages} />
+          </Suspense>
         ) : (
-          <div className="w-full aspect-square skeleton bg-gray-200 flex items-center justify-center mx-auto mt-4 text-gray-400 text-lg font-semibold shadow">
-            No Image
-          </div>
+          <div className="text-center skeleton min-h-[352px]">No Images</div>
         )}
         <div className="flex-1 flex flex-col gap-4">
           <fieldset className="fieldset">
@@ -136,12 +158,21 @@ export default function EditProfileForm({ user }: { user: User }) {
             <input
               type="file"
               ref={inputFileRef}
-              id="image"
-              name="image"
+              name="profileImages"
               accept="image/*"
+              multiple
+              onChange={e => {
+                if (e.target.files && e.target.files.length > MAX_FILES_PER_USER) {
+                  setError(`You can select up to ${MAX_FILES_PER_USER} files only.`);
+                  e.target.value = "";
+                } else {
+                  setError(null);
+                }
+              }}
               className="file-input"
             />
-            <label htmlFor="image" className="label">One image can be selected at a time</label>
+            <label htmlFor="profileImages" className="label">You can upload up to {MAX_FILES_PER_USER} files</label>
+
           </fieldset>
 
           <label className="flex flex-col">
@@ -285,11 +316,11 @@ export default function EditProfileForm({ user }: { user: User }) {
 
           <div className="w-full mx-auto border border-primary h-0"></div>
 
-          <div className="flex flex-col w-full">
+          <div className="flex flex-col w-full max-w-[99.9%]">
             <span className="font-semibold text-gray-700">Socials</span>
             <div className="flex flex-col gap-2 mt-1">
               {socials.map((social, idx) => (
-                <div key={idx} className="flex flex-row flex-wrap gap-2 items-center mb-4">
+                <div key={idx} className="flex flex-row flex-wrap gap-4 items-center mb-4">
                   <input
                     type="text"
                     placeholder="URL"
@@ -303,11 +334,12 @@ export default function EditProfileForm({ user }: { user: User }) {
                   />
                   <div className="flex flex-col w-full gap-2">
                     <div className="flex items-center gap-2">
-                      <span className=" flex items-center justify-center">
+                      <span className=" flex items-center justify-center  max-w-[40px]">
                         {platformIcons[social.platform] || null}
                       </span>
                       <select
                         className="select select-ghost"
+                        name={`socials[${idx}].platform`}
                         value={social.platform}
                         onChange={e => {
                           const updated = [...socials];
@@ -347,6 +379,142 @@ export default function EditProfileForm({ user }: { user: User }) {
             </button>
           </div>
 
+          <div className="w-full mx-auto border border-primary h-0"></div>
+
+          {
+            schedule.map((item, idx) => (
+              <fieldset
+                key={item.id}
+                className="fieldset grid-cols-2 max-w-[99.9%]">
+                <legend className="fieldset-legend ">Work Schedule</legend>
+                <div className="grid grid-cols-1 w-full max-w-[99.9%] gap-2">
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-monday"
+                      value="Monday"
+                      className="checkbox"
+                      checked={item.day.Monday}
+                      onChange={() => toggleScheduleDay(item.id, 'Monday')}
+                    />
+                    Montag
+                  </label>
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-tuesday"
+                      value="Tuesday"
+                      className="checkbox"
+                      checked={item.day.Tuesday}
+                      onChange={() => toggleScheduleDay(item.id, 'Tuesday')}
+                    />
+                    Dienstag
+                  </label>
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-wednesday"
+                      value="Wednesday"
+                      className="checkbox"
+                      checked={item.day.Wednesday}
+                      onChange={() => toggleScheduleDay(item.id, 'Wednesday')}
+                    />
+                    Mittwoch
+                  </label>
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-thursday"
+                      value="Thursday"
+                      className="checkbox"
+                      checked={item.day.Thursday}
+                      onChange={() => toggleScheduleDay(item.id, 'Thursday')}
+                    />
+                    Donnerstag
+                  </label>
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-friday"
+                      value="Friday"
+                      className="checkbox"
+                      checked={item.day.Friday}
+                      onChange={() => toggleScheduleDay(item.id, 'Friday')}
+                    />
+                    Freitag
+                  </label>
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-saturday"
+                      value="Saturday"
+                      className="checkbox"
+                      checked={item.day.Saturday}
+                      onChange={() => toggleScheduleDay(item.id, 'Saturday')}
+                    />
+                    Samstag
+                  </label>
+                  <label htmlFor="schedule" className="label">
+                    <input
+                      type="checkbox"
+                      name="schedule-sunday"
+                      value="Sunday"
+                      className="checkbox"
+                      checked={item.day.Sunday}
+                      onChange={() => toggleScheduleDay(item.id, 'Sunday')}
+                    />
+                    Sonntag
+                  </label>
+                </div>
+                <div className="grid grid-rows-2 gap-3 justify-baseline mt-4 w-full max-w-[99.9%]">
+                  <label htmlFor="time" className="label text-center flex-col">
+                    <span className="label-text">Time start</span>
+                    <div className="flex flex-col">
+                      <input
+                        type="time"
+                        id="time"
+                        name="time-start"
+                        className="border border-primary p-2 rounded"
+                        onChange={(e) => setScheduleTime(item.id, 'open', e.target.value)}
+                      />
+                      <span className="text-xs text-gray-500 w-fit flex flex-nowrap">click here to set <ArrowUpRight /></span>
+                    </div>
+                  </label>
+                  <label htmlFor="time-end" className="label text-center flex-col">
+                    <span className="label-text">Time end</span>
+                    <div className="flex flex-col">
+                      <input
+                        type="time"
+                        id="time-end"
+                        name="time-end"
+                        className="border border-primary p-2 rounded"
+                        onChange={(e) => setScheduleTime(item.id, 'close', e.target.value)}
+                      />
+                      <span className="text-xs text-gray-500 w-fit flex flex-nowrap">click here to set <ArrowUpRight /></span>
+                    </div>
+                  </label>
+                </div>
+                <button
+                  type="button"
+                  onClick={() => setSchedule(schedule.filter((_, i) => i !== idx))}
+                  className="text-red-500 self-end"
+                >
+                  Remove
+                </button>
+              </fieldset>
+
+            ))
+          }
+          <button
+            type="button"
+            onClick={() => setSchedule([...schedule, { id: crypto.randomUUID(), day: { Monday: false, Tuesday: false, Wednesday: false, Thursday: false, Friday: false, Saturday: false, Sunday: false }, open: '', close: '' }])}
+            className="mt-2 px-3 py-1 bg-primary text-white rounded"
+          >
+            Add Schedule
+          </button>
+
+
+          <div className="w-full mx-auto border border-primary h-0"></div>
 
           <button
             type="submit"
