f2cdbb1db1fb8f8795fe8720132f6fc591ea222e
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-07 00:39:36 +0300

    feat: add author profile link and expertise display in NewsCardDetails component

--- app/news/[id]/NewsCardDetails.tsx
+++ app/news/[id]/NewsCardDetails.tsx
@@ -5,6 +5,7 @@ import { NewsCardType } from "@/utils/types";
 import GoBack from "@/components/buttons/go-back/GoBack";
 import FadeCarousel from "@/components/carousels/fade-carousel/FadeCarousel";
 import { Suspense } from "react";
+import Link from "next/link";
 
 export default function NewsCardDetails({ newsData }: { newsData: NewsCardType | null }) {
 
@@ -24,25 +25,35 @@ export default function NewsCardDetails({ newsData }: { newsData: NewsCardType |
           >
             <div className="card card-lg bg-secondary/20 w-96 shadow-sm max-w-[100%]">
               <div className="badge badge-accent rounded-bl-none rounded-tr-none p-4">{createdDate}</div>
-              {author?.name && <div className="text-2xl indent-6 mt-3 text-primary">{author?.name || "Unknown Author"}</div>}
+              {author?.name && <div className="text-2xl indent-6 mt-3 text-primary">
+                <Link
+                  href={`/profile/${author.id}`}
+                  className="hover:underline">
+                  {author?.name || "Unknown Author"}
+                </Link></div>}
+
+              {author?.fieldOfExpertise &&
+                <div className="card-actions mt-1 pt-1 justify-start gap-2 mx-4">
+                  {
+                    author?.fieldOfExpertise.map((expertise, index) => (
+                      <div key={index} className="badge badge-outline">{expertise}</div>
+                    ))}
+                </div>
+              }
+
               <h2 className="card-title card-border flex-col m-4 justify-center">
                 {title}
               </h2>
+
               <div className="card-body">
                 {photos &&
                   <Suspense fallback={<div>Loading...</div>}>
                     <FadeCarousel photos={photos} />
                   </Suspense>
                 }
+
                 <p className="text-base text-pretty indent-4 break-before">{content}</p>
-                {author?.fieldOfExpertise &&
-                  <div className="card-actions mt-1 pt-1 justify-end">
-                    {
-                      author?.fieldOfExpertise.map((expertise, index) => (
-                        <div key={index} className="badge badge-outline">{expertise}</div>
-                      ))}
-                  </div>
-                }
+
               </div>
             </div>
           </div>
