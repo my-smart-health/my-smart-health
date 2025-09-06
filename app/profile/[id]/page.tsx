f20d52102182cc91bd67c2718c8d5a38720bafa6
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-07 00:38:53 +0300

    feat: implement ProfilePage component to display user profile and posts

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,77 @@
+import { auth } from "@/auth";
+import GoBack from "@/components/buttons/go-back/GoBack";
+import ShortPosts from "@/components/posts/short-posts/ShortPosts";
+import ProfileFull from "@/components/profile-full/ProfileFull";
+import prisma from "@/lib/db";
+import { Schedule } from "@/utils/types";
+
+async function getUser(id: string) {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      profileImages: true,
+      address: true,
+      bio: true,
+      phone: true,
+      socials: true,
+      website: true,
+      fieldOfExpertise: true,
+      displayEmail: true,
+      schedule: true,
+    },
+  });
+  return { user };
+}
+
+async function getPosts(userId: string) {
+  const posts = await prisma.posts.findMany({
+    where: { authorId: userId },
+  });
+  return { posts };
+}
+
+function UserNotFound() {
+  return (
+    <>
+      <main
+        className="flex flex-col items-center justify-center gap-3 w-full mb-auto max-w-[100%]">
+        <div>User not found</div>
+        <GoBack />
+      </main>
+    </>
+  );
+}
+
+export default async function ProfilePage({ params }: { params: Promise<{ id: string }> }) {
+  const session = await auth();
+
+  const safeSession = session ? session : null;
+
+  const { id } = await params;
+  const { user } = await getUser(id);
+  const { posts } = await getPosts(id);
+  if (!user || user === null) {
+    return (
+      <UserNotFound />
+    );
+  }
+  const safeSchedule = user.schedule ? user.schedule as Schedule[] : [];
+  const safeUser = { ...user, schedule: safeSchedule };
+
+  return (
+    <>
+      <main
+        className="flex flex-col gap-4 items-center min-h-[72dvh] py-8 max-w-[99.9%] text-wrap break-normal overflow-clip overscroll-none">
+        <ProfileFull user={safeUser} />
+        <GoBack />
+        <section className="flex flex-col w-full rounded-2xl shadow-md">
+          <div className="font-semibold text-primary text-2xl text-center">Posts</div>
+          {posts ? <ShortPosts posts={posts} session={safeSession} /> : <div>No posts available</div>}
+        </section>
+      </main>
+    </>
+  );
+}
