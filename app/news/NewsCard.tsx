ce460fac4ff1ad6169e97f5d12db696753a9367e
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-07 00:39:02 +0300

    feat: enhance NewsCard component with author profile link and expertise display

--- app/news/NewsCard.tsx
+++ app/news/NewsCard.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+import Link from "next/link";
+import { Suspense } from "react";
+
 import { NewsCardType } from "@/utils/types";
 
 import GoToButton from "@/components/buttons/go-to/GoToButton";
 import FadeCarousel from "@/components/carousels/fade-carousel/FadeCarousel";
-import { Suspense } from "react";
 
 export default function NewsCard({ newsData }: { newsData: NewsCardType[] }) {
   return (
@@ -24,8 +26,23 @@ export default function NewsCard({ newsData }: { newsData: NewsCardType[] }) {
             className="m-auto min-h-full border max-w-[99%] rounded-lg shadow-2xl"
           >
             <div className="card card-lg bg-secondary/20 w-96 shadow-sm max-w-[100%]">
+
               <div className="badge badge-accent rounded-bl-none rounded-tr-none p-4">{createdDate}</div>
-              {author?.name && <div className="text-2xl indent-6 mt-3 text-primary">{author?.name || "Unknown Author"}</div>}
+
+              {author?.name && <div className="text-2xl indent-6 mt-3 text-primary ">
+                <Link
+                  href={`/profile/${author.id}`}
+                  className="hover:underline">
+                  {author?.name || "Unknown Author"}
+                </Link>
+              </div>}
+
+              <div className="card-actions mt-1 pt-1 justify-start gap-2 mx-4">
+                {
+                  author?.fieldOfExpertise.map((expertise, index) => (
+                    <div key={index} className="badge badge-outline">{expertise}</div>
+                  ))}
+              </div>
               <h2 className="card-title card-border flex-col m-4 justify-center">
                 {title}
               </h2>
@@ -34,12 +51,7 @@ export default function NewsCard({ newsData }: { newsData: NewsCardType[] }) {
                   <FadeCarousel photos={photos} />
                 </Suspense>
                 <p className="text-base line-clamp-3 indent-6">{content}</p>
-                <div className="card-actions justify-end">
-                  {
-                    author?.fieldOfExpertise.map((expertise, index) => (
-                      <div key={index} className="badge badge-outline">{expertise}</div>
-                    ))}
-                </div>
+
                 <div className="card-actions justify-center">
                   <GoToButton name={"View more"} src={`/news/${id}`} className="btn btn-wide bg-primary rounded-xl text-secondary" />
                 </div>
