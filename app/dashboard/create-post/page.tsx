565de0de8329cad08bdfd1885ef34a60ea1e9baa
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-09 19:43:45 +0300

    feat: add CreatePostPage component for creating new posts

---
+++ app/dashboard/create-post/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/auth";
+import CreatePostForm from "./CreatePostForm";
+import { redirect } from "next/navigation";
+
+export default async function CreatePostPage() {
+  const session = await auth();
+  if (!session) {
+    redirect("/login");
+  }
+
+  return (
+    <main className="flex flex-col gap-4 h-full min-h-[72dvh] max-w-[90%]">
+      <h1 className="text-4xl font-bold text-primary text-center">New Post</h1>
+      <CreatePostForm session={session} />
+    </main>
+  );
+}
