e46cc18bdb96a7bc71f5a84c6f0ee1403a27b03c
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-09 17:12:30 +0300

    fix: adjust padding and spacing in ProfileShort component for improved layout

--- components/profile-short/ProfileShort.tsx
+++ components/profile-short/ProfileShort.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function ProfileShort({ id, name, bio, image }: { id: string; name: string; bio: string; image: string }) {
   return (
     <>
-      <div className="flex flex-col p-3 gap-1 border border-dashed rounded-lg bg-white/80 shadow-md hover:shadow-xl transition-shadow">
+      <div className="flex flex-col p-4 gap-1 border border-dashed rounded-lg bg-white/80 shadow-md hover:shadow-xl transition-shadow">
         <h3 className="font-bold text-lg">{name}</h3>
         <div className="flex h-full">
           <Image src={image} alt={name} width={96} height={96} className=" w-full h-full aspect-square rounded-lg" />
         </div>
-        <p className="text-gray-600 line-clamp-3">{bio}</p>
+        <p className="text-gray-600 line-clamp-3 pt-3 pb-4">{bio}</p>
       </div>
       <span className="flex justify-end">
         <Link href={`/profile/${id}`} className="btn btn-sm btn-primary self-end mt-2">
