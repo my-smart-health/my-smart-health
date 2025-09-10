32b5ceb956551f2caf183070ff54a5a29985bdf8
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-10 03:16:47 +0300

    feat: add CreatePostForm component for creating new posts with media support

---
+++ app/dashboard/create-post/CreatePostForm.tsx
@@ -0,0 +1,307 @@
+"use client"
+
+import { Session } from "next-auth";
+import { redirect } from "next/navigation";
+import { PutBlobResult } from "@vercel/blob";
+import { FormEvent, MouseEvent, useEffect, useRef, useState } from "react";
+
+import { MAX_FILES_PER_POST } from "@/utils/constants";
+import GoToButton from "@/components/buttons/go-to/GoToButton";
+
+import logo from '@/public/og-logo.jpg';
+import { ArrowUpRight } from "lucide-react";
+import YoutubeEmbed from "@/components/embed/youtube/YoutubeEmbed";
+import InstagramEmbed from "@/components/embed/instagram/InstagramEmbed";
+import Image from "next/image";
+import MoveImageVideo from "../../../components/buttons/move-up-down-image-video/MoveImageVideo";
+import GoBack from "@/components/buttons/go-back/GoBack";
+
+type CreatePostFormProps = {
+  session: Session | null;
+};
+
+export default function CreatePostForm({ session }: CreatePostFormProps) {
+
+  if (!session) {
+    redirect('/login');
+  }
+
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const [isDefaultLogo, setIsDefaultLogo] = useState<boolean>(false);
+
+  const [blobResult, setBlobResult] = useState<string[]>([]);
+
+  const [isImageFirst, setIsImageFirst] = useState<boolean>(true);
+
+  useEffect(() => {
+    blobResult.length > 0
+      && setIsImageFirst((blobResult[0].search("instagram") === -1
+        && blobResult[0].search("youtube") === -1
+        && blobResult[0].search("youtu") === -1)
+        ? true : false);
+  }, [blobResult]);
+
+  const handleAddURL = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget.closest('form');
+    if (!form) return;
+    const formData = new FormData(form);
+    const mediaUrl = formData.get(`media`)?.toString().trim();
+
+    if (!mediaUrl || mediaUrl.length === 0) {
+      setError('Media URL cannot be empty');
+      return;
+    }
+    if (
+      /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]+/.test(mediaUrl) ||
+      /^(https?:\/\/)?(www\.)?instagram\.com\/(reel|p)\/[\w-]+/.test(mediaUrl)
+    ) {
+      setError(null);
+      setBlobResult([...blobResult, mediaUrl]);
+      const resetMediaInput = form.querySelector('input[name="media"]') as HTMLInputElement;
+      resetMediaInput.value = '';
+    } else {
+      setError('Media URL must be a valid YouTube or Instagram link');
+      return;
+    }
+  };
+
+  const handleImageUpload = async (file: File) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      if (file === undefined) {
+        setIsDefaultLogo(true);
+        return logo.src;
+      }
+
+      const uniqueFileName = `${Date.now()}-${file.name}`;
+      const response = await fetch(
+        `/api/upload-picture/?userid=${session.user.id}&filename=${uniqueFileName}`,
+        {
+          method: 'POST',
+          body: file,
+        }
+      );
+
+      const result = await response.json() as PutBlobResult;
+
+      if (!response.ok) {
+        setError('Failed to upload image');
+        setIsDisabled(false);
+        throw new Error('Failed to upload image');
+      }
+
+      return result.url;
+    } catch (error) {
+      let message = 'Error uploading files';
+      if (error instanceof Error) {
+        message = error.message;
+      }
+      setError(message);
+      setIsDisabled(false);
+      return logo.src;
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+
+    try {
+      event.preventDefault();
+      setIsDisabled(true);
+      const formData = new FormData(event.currentTarget);
+
+      if (blobResult.length === 0) {
+        setIsDefaultLogo(true);
+        blobResult.push(logo.src);
+      }
+
+      if (!isImageFirst) {
+        setError("First media must be an image");
+        setIsDisabled(false);
+        return;
+      }
+      setError(null);
+
+      const result = await fetch(`/api/create-post`, {
+        method: 'POST',
+        body: JSON.stringify({
+          authorId: session.user.id,
+          title: formData.get('title'),
+          content: formData.get('content'),
+          photos: blobResult,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!result.ok) {
+        setError('Failed to create post');
+        setIsDisabled(false);
+        return;
+      }
+
+      setError("Post created successfully");
+      setIsDisabled(true);
+    } catch (error) {
+      let message = 'Error uploading files';
+      if (error instanceof Error) {
+        message = error.message;
+      }
+      setError(message);
+      setIsDisabled(false);
+      return;
+    }
+  };
+
+  return (
+    <>
+      <span className="self-end mb-4">
+        <GoBack />
+      </span>
+      {error && <p className={`${error === 'Post created successfully' ? 'text-green-500' : 'text-red-500'} bg-secondary/10 border-2 border-primary rounded-2xl p-2 text-center break-all`}>{error}</p>}
+      {isDefaultLogo && <div className="flex flex-col items-center">
+        <p className="text-yellow-500 bg-secondary/10 border-2 border-primary rounded-2xl p-2 text-center break-all">
+          No image file was selected, default logo will be used
+        </p>
+        <p className="text-sm text-gray-500 italic">
+          You can change it later in the edit post section
+        </p>
+      </div>}
+      <form
+        onSubmit={handleSubmit}
+        className={`flex flex-col gap-4 ${isDisabled ? 'opacity-50 pointer-events-none' : ''}`}>
+        <fieldset className="fieldset text-lg">
+          <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+            Title
+            <textarea
+              id="title"
+              name="title"
+              required
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+            />
+            <div className="label pt-1 text-xs flex flex-row justify-end">You can pull the right corner to resize it <ArrowUpRight /></div>
+          </label>
+        </fieldset>
+        <fieldset className="fieldset">
+          <label htmlFor="content" className="block text-sm font-medium text-gray-700">
+            Content
+            <textarea
+              id="content"
+              name="content"
+              required
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+            />
+            <div className="label pt-1 text-xs flex flex-row justify-end">You can pull the right corner to resize it <ArrowUpRight /></div>
+          </label>
+        </fieldset>
+
+        <span className={blobResult.length >= MAX_FILES_PER_POST ? 'opacity-50 pointer-events-none' : ''}>
+          <fieldset>
+            <legend className="fieldset-legend">Select File</legend>
+            <div className="flex flex-wrap gap-4 w-full">
+              <input
+                type="file"
+                ref={inputFileRef}
+                id="image"
+                name="image"
+                accept="image/*"
+                className={`${blobResult && blobResult.length >= MAX_FILES_PER_POST ? 'opacity-50 pointer-events-none' : ''} file-input`}
+                onChange={async e => {
+                  if (e.target.files && e.target.files.length > MAX_FILES_PER_POST) {
+                    setError(`You can select up to ${MAX_FILES_PER_POST} files only.`);
+                    e.target.value = "";
+                  } else {
+                    inputFileRef.current = e.target;
+                    const uploadedImage = await handleImageUpload(e.target.files?.[0] as File);
+                    setBlobResult(prev => [...prev, uploadedImage as string]);
+                    setError(null);
+                  }
+                }} />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend className="fieldset-legend">Add Media URL</legend>
+            <div className="flex flex-col gap-4 w-full">
+              <input
+                type="text"
+                name={`media`}
+                placeholder="https://"
+                className="p-3 rounded border border-gray-300 text-base focus:outline-none focus:ring-2 focus:ring-primary w-full"
+              />
+              <label htmlFor={`media`} className="">Media URL must be a valid URL from YouTube or Instagram</label>
+              <button
+                type="button"
+                onClick={(e) => {
+                  handleAddURL(e)
+                }}
+                className="btn w-full btn-primary mt-2"
+              >
+                Upload Media URL
+              </button>
+            </div>
+          </fieldset>
+        </span>
+
+        <div className="flex flex-col self-center w-full max-w-[90%] gap-4">
+          {blobResult && blobResult.map((image, idx) => {
+            const media = image.includes("youtube") || image.includes("youtu")
+              ? <YoutubeEmbed embedHtml={image} width={200} height={200} />
+              : image.includes("instagram")
+                ? <InstagramEmbed embedHtml={image} width={200} height={200} />
+                : <Image
+                  src={image}
+                  alt={`Photo ${idx + 1}`}
+                  width={200}
+                  height={200}
+                  placeholder="empty"
+                  className="object-cover rounded-lg w-[200px] h-[200px] hover:z-10 hover:scale-200 hover:shadow-lg cursor-pointer transition-all"
+                />;
+
+            return (
+              <div
+                key={image + idx}
+                className="flex w-full justify-center items-center gap-4 max-w-[90%]"
+                style={{ minHeight: 200 }}
+              >
+                <div className="flex items-center justify-center w-[200px] h-[200px]">
+                  {media}
+                </div>
+                <div className="flex flex-col items-center justify-center h-[200px] gap-2">
+                  <MoveImageVideo
+                    index={idx}
+                    blobResult={blobResult}
+                    setBlobResult={setBlobResult}
+                    showTop={idx > 0}
+                    showBottom={idx < blobResult.length - 1}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        {
+          blobResult && blobResult.length > 0 && (
+            <div className="space-y-2">
+              <div className="text-sm">You can add up to {MAX_FILES_PER_POST} media files (images, Instagram videos, or YouTube videos)</div>
+              <p className="text-wrap text-warning">NB: Please ensure that the first media is an image.</p>
+            </div>
+          )}
+
+        <button type="submit" className={`p-2 rounded bg-primary text-white font-bold text-base hover:bg-primary/80 transition-colors ${isImageFirst ? '' : 'opacity-50 pointer-events-none'}`}>
+          Create Post
+        </button>
+      </form >
+      <div className="text-xl">
+        {isDisabled && <GoToButton src="/dashboard" name="Back to Dashboard" />}
+      </div>
+    </>
+  );
+}
