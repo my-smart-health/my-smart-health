61f945dc20ab129c676666760106f1f5d27ea8e0
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-10 03:16:32 +0300

    feat: implement PUT endpoint for updating posts in API

---
+++ app/api/update-post/route.ts
@@ -0,0 +1,34 @@
+import prisma from '@/lib/db';
+import { NextResponse } from 'next/server';
+
+export async function PUT(req: Request) {
+  try {
+    const { id, title, content, photos } = await req.json();
+
+    if (!id || !title || !content) {
+      return NextResponse.json(
+        { error: 'Missing required fields' },
+        { status: 400 }
+      );
+    }
+
+    const updatedPost = await prisma.posts.update({
+      where: { id: id },
+      data: {
+        title,
+        content,
+        photos,
+      },
+    });
+
+    return NextResponse.json(updatedPost);
+  } catch (error) {
+    if (process.env.NODE_ENV === 'development') {
+      console.error(error);
+    }
+    return NextResponse.json(
+      { error: 'Failed to update post' },
+      { status: 500 }
+    );
+  }
+}
