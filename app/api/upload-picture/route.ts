6c3e0e6b74dd62082f72f61ec0745d7a8e83f84d
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-11 21:51:50 +0300

    fix: update file upload logic to use unique filename for preventing overwrites

--- app/api/upload-picture/route.ts
+++ app/api/upload-picture/route.ts
@@ -23,8 +23,9 @@ export async function POST(request: Request) {
     if (!request.body) {
       return NextResponse.json({ error: 'File is required' }, { status: 400 });
     }
+    const uniqueFileName = `${Date.now()}-${filename}`;
 
-    const blob = await put(`${userid}/news/${filename}`, request.body, {
+    const blob = await put(`${userid}/news/${uniqueFileName}`, request.body, {
       access: 'public',
       allowOverwrite: true,
     });
