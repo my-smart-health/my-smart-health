191501921b5d19016f422d7ed204cdb3d8d9869c
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-11 15:31:56 +0300

    feat: integrate Youtube and Instagram embeds in ProfilePictureCarousel for enhanced media support

--- components/carousels/profile-picture-carousel/ProfilePictureCarousel.tsx
+++ components/carousels/profile-picture-carousel/ProfilePictureCarousel.tsx
@@ -11,6 +11,8 @@ import 'swiper/css/scrollbar';
 import 'swiper/css/autoplay';
 import 'swiper/css/mousewheel';
 import 'swiper/css/effect-cards';
+import YoutubeEmbed from "@/components/embed/youtube/YoutubeEmbed";
+import InstagramEmbed from "@/components/embed/instagram/InstagramEmbed";
 
 
 export default function ProfilePictureCarousel({ imageSrcArray, disableOnInteraction = false }: { imageSrcArray?: string[]; disableOnInteraction?: boolean }) {
@@ -24,31 +26,60 @@ export default function ProfilePictureCarousel({ imageSrcArray, disableOnInterac
       <Swiper
         modules={[Scrollbar, Mousewheel, Autoplay, EffectCards]}
         spaceBetween={1}
+        lazyPreloadPrevNext={3}
         slidesPerView={1}
-
-        speed={300}
-
-        autoplay={{ delay: 3000, disableOnInteraction: disableOnInteraction, pauseOnMouseEnter: true, waitForTransition: true }}
-        mousewheel={true}
-        scrollbar={{ draggable: true }}
         effect="cards"
+        cardsEffect={{
+          slideShadows: false,
+        }}
+        grabCursor={true}
+        mousewheel={true}
+        autoplay={{ delay: 3000, disableOnInteraction: disableOnInteraction, pauseOnMouseEnter: true, waitForTransition: true }}
+        speed={300}
+        scrollbar={{ draggable: true }}
         className="max-w-2xs xs:max-w-3xs md:max-w-sm mx-auto"
       >
-        {imageSrcArray && imageSrcArray.map((image, idx) => (
-          <SwiperSlide key={idx} className="my-4">
-            <div className="rounded-box cursor-pointer">
-              <Image
-                loading="lazy"
-                placeholder="empty"
-                width={350}
-                height={350}
-                src={image}
-                alt="Health Bar"
-                className="aspect-square"
-              />
-            </div>
-          </SwiperSlide>
-        ))}
+        {imageSrcArray && imageSrcArray.map((image, idx) => {
+
+          const WIDTH = 350;
+          const HEIGHT = 350;
+          if (image.search('youtu') > 0 || image.search('youtube') > 0) {
+            return (
+              <SwiperSlide key={idx}>
+                <div className="flex flex-col justify-center items-center aspect-video rounded-box cursor-pointer max-w-full">
+                  <YoutubeEmbed embedHtml={image} width={WIDTH} height={HEIGHT} />
+                </div>
+                <br />
+              </SwiperSlide>
+            )
+          }
+          if (image.search('instagram') > 0) {
+            return (
+              <SwiperSlide key={idx}>
+                <div className="flex flex-col justify-center items-center rounded-box cursor-pointer max-w-full">
+                  <InstagramEmbed embedHtml={image} width={WIDTH} height={HEIGHT} />
+                </div>
+                <br />
+              </SwiperSlide>
+            )
+          } else {
+            return (
+              <SwiperSlide key={idx} className="my-4">
+                <div className="rounded-box cursor-pointer">
+                  <Image
+                    loading="lazy"
+                    placeholder="empty"
+                    width={WIDTH}
+                    height={HEIGHT}
+                    src={image}
+                    alt={image}
+                    className="aspect-square"
+                  />
+                </div>
+              </SwiperSlide>
+            );
+          }
+        })}
       </Swiper>
     </Suspense>
   );
