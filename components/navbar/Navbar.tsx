15454b7e4e542dd72fdf07fa40448f3df156ae78
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-13 19:22:58 +0300

    fix: refactor Navbar component for improved structure and styling

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,42 +1,42 @@
 import Image from "next/image";
 import Link from "next/link";
-import NotrufDropdown from "./NotrufDropdown";
 import { Suspense } from "react";
 import ProfileMenu from "./ProfileMenu";
 
-export default async function Navbar() {
-
+export default function Navbar() {
   return (
     <>
       <nav
         draggable={false}
-        className="flex flex-col mx-auto mb-1 w-full max-w-[90%]"
+        className="flex flex-col mx-auto w-full max-w-[90%]"
       >
-        <div className="flex flex-row gap-1 items-center justify-evenly text-start text-nowrap">
+        <div className="flex flex-row gap-1 items-center justify-evenly">
           <Link
             draggable={false}
             href="/"
-            className="flex flex-col items-start sm:items-center"
+            className="flex items-center"
           >
-            <div className="flex flex-row items-center justify-center text-xl font-bold uppercase">
-              <Image
-                priority
-                loading="eager"
-                draggable={false}
-                src="/og-logo.jpg"
-                alt="My Smart Health"
-                width={350}
-                height={112}
-                className="w-96 h-auto mx-auto"
-              />
-            </div>
+            <Image
+              priority
+              loading="eager"
+              draggable={false}
+              src="/og-logo.jpg"
+              alt="My Smart Health"
+              width={350}
+              height={112}
+              className="w-96 h-auto mx-auto"
+            />
+            <span className="ml-2 text-xl font-bold uppercase sr-only">My Smart Health</span>
           </Link>
-          <NotrufDropdown />
         </div>
       </nav>
-      <Suspense fallback={<div className="skeleton flex flex-row justify-evenly align-baseline h-full max-h-fit w-full max-w-[100%] gap-2 bg-primary text-white p-2 mb-4">Loading...</div>}>
+      <Suspense fallback={
+        <div className="skeleton flex justify-evenly items-center h-12 w-full bg-primary text-white p-2 mb-4">
+          Loading...
+        </div>
+      }>
         <ProfileMenu />
       </Suspense>
     </>
-  )
+  );
 }
