4e2a8d1e69c8e51418be8cc2ebbbe8e1c44c1589
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-15 18:46:44 +0300

    fix: update post fetching logic to use select instead of include and add fieldOfExpertise to author details

--- app/profile/[id]/news/page.tsx
+++ app/profile/[id]/news/page.tsx
@@ -1,22 +1,31 @@
 import NewsCardDetails from "@/app/news/NewsCardDetails";
 import { auth } from "@/auth";
 import prisma from "@/lib/db";
+import { NewsCardType } from "@/utils/types";
 
 async function getAllPostsByUserId(userId: string) {
   const posts = await prisma.posts.findMany({
     where: { authorId: userId },
     orderBy: { createdAt: "desc" },
-    include: {
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      createdAt: true,
+      updatedAt: true,
+      photos: true,
+      tags: true,
       author: {
         select: {
           id: true,
           name: true,
+          fieldOfExpertise: true,
         }
       }
     }
   });
 
-  return posts;
+  return posts as NewsCardType[];
 }
 
 async function getAuthorNameById(authorId: string) {
@@ -44,13 +53,13 @@ export default async function ProfileNewsPage({ params }: { params: Promise<{ id
         <h1 className="flex flex-wrap mx-auto text-xl font-bold text-primary text-center">{authorName} News</h1>
 
         {posts.length > 0 && session && (
-          posts.map((post: any) => (
+          posts.map((post) => (
             <NewsCardDetails key={post.id} newsData={post} session={session} />
           ))
         )}
 
         {posts.length > 0 && !session && (
-          posts.map((post: any) => (
+          posts.map((post) => (
             <NewsCardDetails key={post.id} newsData={post} />
           ))
         )}
