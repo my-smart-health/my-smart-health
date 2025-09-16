03f0aec68b9943bc08a79f729d6c11e0bc104298
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-16 14:59:13 +0300

    fix: update LogOut button styling for improved visibility and consistency

--- components/buttons/log-out/LogOut.tsx
+++ components/buttons/log-out/LogOut.tsx
@@ -5,7 +5,7 @@ import { signOut } from "next-auth/react";
 export default function LogOut() {
   return (
     <button
-      className="btn btn-neutral rounded-2xl bg-red-500/85 hover:bg-red-600/85"
+      className="btn btn-outline btn-error hover:text-white"
       onClick={() => signOut({ redirect: true, redirectTo: '/' })}>
       Logout
     </button>
