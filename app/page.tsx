c1214f553db77c93010d91268ad89f40fcc493f7
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-16 19:57:24 +0300

    refactor: enhance news filtering and structure in Home component for better data handling

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ async function getNews() {
       title: true,
       photos: true,
       createdAt: true,
+      author: true,
     }
   });
   return { news };
@@ -24,7 +25,13 @@ export default async function Home() {
   const { news } = await getNews();
 
   const newsTopCarousel = news.length > 0
-    ? news.map(item => ({ id: item.id, name: item.title, profileImage: item.photos[0] }))
+    ? news
+      .filter(item => item.author && item.author.name && item.id && item.photos && item.photos[0])
+      .map(item => ({
+        id: item.id,
+        name: item.author.name as string,
+        profileImage: item.photos[0]
+      }))
     : undefined;
 
   const safeNews = news.length > 0 ? news.map(item => ({
@@ -35,10 +42,12 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-center gap-4 max-w-[100%] min-h-[100dvh] mb-auto justify-items-center">
-      <span className="w-full border">
+      <div className="w-full">
         <TopCarousel props={newsTopCarousel} />
-      </span>
-      {safeNews && <NewsCarousel props={safeNews} />}
+      </div>
+      <div className="w-full">
+        {safeNews && <NewsCarousel props={safeNews} />}
+      </div>
       <div className="flex flex-col gap-3 w-full mx-auto max-w-[100%]">
         <NewsSmartHealthMedizinButton name="News" icon="/icon2.png" goTo="/news" />
         <NewsSmartHealthMedizinButton name="Smart Health" icon="/icon3.png" goTo="/smart-health" />
