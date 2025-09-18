096a2355410cc0333ac46808e6e79b5843d6843a
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-18 21:28:40 +0300

    feat: implement user and post retrieval functions in common.ts

--- utils/common.ts
+++ utils/common.ts
@@ -1,3 +1,4 @@
+import prisma from '@/lib/db';
 import { Social } from './types';
 
 export function parseSocials(socials: string[]): Social[] {
@@ -13,4 +14,40 @@ export function serializeSocials(socials: Social[]): string[] {
   return socials
     .filter((s) => s.platform && s.url)
     .map((s) => `${s.platform}|${s.url}`);
+}
+
+export async function getUser(id: string) {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      profileImages: true,
+      address: true,
+      bio: true,
+      phone: true,
+      socials: true,
+      website: true,
+      fieldOfExpertise: true,
+      displayEmail: true,
+      schedule: true,
+      certificates: true,
+    },
+  });
+  return { user };
+}
+
+export async function getAllPosts(userId: string) {
+  const posts = await prisma.posts.findMany({
+    where: { authorId: userId },
+    select: {
+      id: true,
+      title: true,
+      photos: true,
+      authorId: true,
+    },
+  });
+
+  return posts;
 }
