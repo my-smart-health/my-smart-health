f3343a05a255fb77a91887b06fdb53a91134dda5
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-18 03:22:02 +0300

    feat: implement PUT endpoint for uploading certificate images with validation

---
+++ app/api/upload/upload-certificate-images/route.ts
@@ -0,0 +1,39 @@
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
+
+export async function PUT(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+    const filename = searchParams.get('filename');
+    const userid = searchParams.get('userid');
+
+    if (!userid) {
+      return NextResponse.json(
+        { error: 'User ID is required' },
+        { status: 400 }
+      );
+    }
+
+    if (!filename) {
+      return NextResponse.json(
+        { error: 'Filename is required' },
+        { status: 400 }
+      );
+    }
+    if (!request.body) {
+      return NextResponse.json({ error: 'File is required' }, { status: 400 });
+    }
+
+    const blob = await put(`${userid}/certificates/${filename}`, request.body, {
+      access: 'public',
+      addRandomSuffix: true,
+    });
+
+    return NextResponse.json(blob, { status: 200 });
+  } catch (error) {
+    if (process.env.NODE_ENV === 'development') {
+      console.error('Error uploading picture:', error);
+    }
+    return NextResponse.json({ error: 'Invalid request' }, { status: 400 });
+  }
+}
