035667ab9b821b3508e96aa7219f47d327855dd0
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-19 00:30:28 +0300

    refactor: remove unused prisma imports and user/post retrieval functions from common.ts

--- utils/common.ts
+++ utils/common.ts
@@ -1,4 +1,3 @@
-import prisma from '@/lib/db';
 import { Social } from './types';
 
 export function parseSocials(socials: string[]): Social[] {
@@ -14,40 +13,4 @@ export function serializeSocials(socials: Social[]): string[] {
   return socials
     .filter((s) => s.platform && s.url)
     .map((s) => `${s.platform}|${s.url}`);
-}
-
-export async function getUser(id: string) {
-  const user = await prisma.user.findUnique({
-    where: { id },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      profileImages: true,
-      address: true,
-      bio: true,
-      phone: true,
-      socials: true,
-      website: true,
-      fieldOfExpertise: true,
-      displayEmail: true,
-      schedule: true,
-      certificates: true,
-    },
-  });
-  return { user };
-}
-
-export async function getAllPosts(userId: string) {
-  const posts = await prisma.posts.findMany({
-    where: { authorId: userId },
-    select: {
-      id: true,
-      title: true,
-      photos: true,
-      authorId: true,
-    },
-  });
-
-  return posts;
 }
