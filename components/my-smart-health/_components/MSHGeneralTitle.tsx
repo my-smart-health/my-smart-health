462a2f131c48cbaee53e9ad8a15098cbf7770782
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-25 02:14:16 +0300

    feat: add MSHGeneralTitle component for inputting general titles

---
+++ components/my-smart-health/_components/MSHGeneralTitle.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+export default function MSHGeneralTitle({ generalTitle, setGeneralTitleAction }: { generalTitle: string; setGeneralTitleAction: (title: string) => void }) {
+
+  return (
+    <label>
+      General Title:
+      <textarea
+        name="generalTitle"
+        value={generalTitle}
+        onChange={(e) => setGeneralTitleAction(e.target.value)}
+        required
+        className="p-3 rounded border border-primary text-base focus:outline-none focus:ring-2 focus:ring-primary w-full"
+      />
+    </label>
+  );
+}
