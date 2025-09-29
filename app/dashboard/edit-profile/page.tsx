29639dc08d0ee5ff0cc6b6d4fc304ac35973d560
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-30 02:22:46 +0300

    refactor: reorganize imports and remove admin profile edit link from EditProfile component

--- app/dashboard/edit-profile/page.tsx
+++ app/dashboard/edit-profile/page.tsx
@@ -1,9 +1,10 @@
-import { Session } from "next-auth";
-import { redirect } from "next/navigation";
-import EditProfileForm from "../../../components/profile/edit-profile-form/EditProfileForm";
 import { auth } from "@/auth";
+import { Session } from "next-auth";
+
 import prisma from "@/lib/db";
-import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import EditProfileForm from "@/components/profile/edit-profile-form/EditProfileForm";
 
 async function getData(sessionId: string) {
   const user = await prisma.user.findUnique({
@@ -45,20 +46,6 @@ export default async function EditProfile() {
     <>
       <h1 className="text-4xl font-extrabold text-primary mb-6">Edit Profile</h1>
       <EditProfileForm user={parsedUser} />
-      {
-        session?.user?.role === "ADMIN" && parsedUser.id !== session.user.id && (
-          <div
-            className="text-sm text-gray-500 italic mt-4">
-            <Link
-              href={`/dashboard/edit-profile/${user.id}`}
-              className="btn btn-warning underline">
-              Edit profile
-            </Link>
-            <p className="mt-2">
-              Note: You are logged in as an admin. You can edit any user&apos;s profile from the admin dashboard.
-            </p>
-          </div>
-        )}
     </>
   );
 }
