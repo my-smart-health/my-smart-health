8d55c6d5d261dd07324bbe6269b699a0c1f73226
Radoslav-Marinovv <radoslav.marinov89@gmail.com>
2025-09-30 02:23:02 +0300

    refactor: simplify ProfileMediaCarousel component by removing Suspense and optimizing conditional rendering

--- components/profile/edit-profile-form/_components/ProfileMediaCarousel.tsx
+++ components/profile/edit-profile-form/_components/ProfileMediaCarousel.tsx
@@ -1,19 +1,14 @@
-import { Suspense } from "react";
 import FadeCarousel from "@/components/carousels/fade-carousel/FadeCarousel";
-import { MAX_FILES_PER_USER } from "@/utils/constants";
 
 export function ProfileMediaCarousel({ blobResult }: { blobResult: string[] }) {
+
   return (
     <section>
       <div className="flex flex-col justify-center items-center content-center max-w-full">
-        {blobResult && blobResult.length > 0 ? (
-          <div className="max-w-full">
-            <Suspense fallback={<div className={`text-center skeleton min-h-[${MAX_FILES_PER_USER}]`}>Loading...</div>}>
-              <FadeCarousel photos={blobResult} />
-            </Suspense>
-          </div>
+        {blobResult.length > 0 ? (
+          <FadeCarousel photos={blobResult} />
         ) : (
-          <div className="text-center skeleton min-h-[352px]">No Images</div>
+          <div className="flex justify-center items-center text-center skeleton min-h-80 w-full font-bold">No Images</div>
         )}
       </div>
     </section>
