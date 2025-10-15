ebcfbf482180bf7bb9010c5a3880d32b79a11445
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-10-15 13:57:38 +0300

    refactor: update icon prop type to support React nodes in NewsSmartHealthMedizinButton

--- components/buttons/news-smart-health-medizin-button/NewsSmartHealthMedizinButton.tsx
+++ components/buttons/news-smart-health-medizin-button/NewsSmartHealthMedizinButton.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
 type NewsSmartHealthMedizinButtonProps = {
   name: string;
-  icon: string;
+  icon: string | React.ReactNode;
   goTo: string;
   active?: boolean;
 };
@@ -12,7 +13,11 @@ export default function NewsSmartHealthMedizinButton({ name, icon, goTo, active 
   return (
     <Link href={goTo} className="flex flex-row h-15 w-full max-w-[100%] rounded-2xl border shadow-xl">
       <div className="w-14 rounded-tl-2xl rounded-bl-2xl flex items-center p-2 justify-center bg-primary text-white">
-        <Image priority loading="eager" src={icon} width={26} height={26} alt={`${name} icon`} style={{ objectFit: "contain", width: "auto", height: "auto" }} className="w-7 h-7 bg-primary text-white" />
+        {typeof icon === "string" ? (
+          <Image priority loading="eager" src={icon} width={26} height={26} alt={`${name} icon`} style={{ objectFit: "contain", width: "auto", height: "auto" }} className="w-7 h-7 bg-primary text-white" />
+        ) : (
+          icon
+        )}
       </div>
       <div className={`flex items-center justify-start pl-2.5 font-bold text-xl text-[#2c2e35]  w-full ${active && 'bg-secondary rounded-br-2xl rounded-tr-2xl'}`}>{name}</div>
     </Link>
