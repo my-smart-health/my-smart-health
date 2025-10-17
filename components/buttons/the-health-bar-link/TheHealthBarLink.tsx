31c38f72a279986c5c62a4980bf8be752e0136a5
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-10-17 16:38:24 +0300

    refactor: remove unused Image import in TheHealthBarLink component

--- components/buttons/the-health-bar-link/TheHealthBarLink.tsx
+++ components/buttons/the-health-bar-link/TheHealthBarLink.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function TheHealthBarLink() {
   return (
     <>
-
       <Link
         draggable={false}
         href="/the-health-bar"
