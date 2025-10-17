4eff7164c35ce94c81bac5b20a9cb139c1572d94
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-10-17 14:46:17 +0300

    fix: adjust SVG height property for consistent rendering in TheHealthBarLink

--- components/buttons/the-health-bar-link/TheHealthBarLink.tsx
+++ components/buttons/the-health-bar-link/TheHealthBarLink.tsx
@@ -13,7 +13,7 @@ export default function TheHealthBarLink() {
       >
         <div className="w-full flex justify-center">
           <div className="w-full max-w-full border border-primary rounded p-6 mt-6 text-center">
-            <svg viewBox="0 0 1000 200" width="100%" height="auto" preserveAspectRatio="xMidYMid meet" role="img" aria-label="the Health Bar">
+            <svg viewBox="0 0 1000 200" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" role="img" aria-label="the Health Bar">
               <defs>
                 <style>{".t{fill:#20b7b0;font-family:'Playfair Display',Georgia,'Times New Roman',serif;font-weight:700;}"}</style>
               </defs>
