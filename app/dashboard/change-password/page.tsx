12a8a35b5f970977371436058ef3e8cdce8c902e
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-10-23 14:40:18 +0300

    feat: simplify ChangePasswordPage by removing user fetching logic

--- app/dashboard/change-password/page.tsx
+++ app/dashboard/change-password/page.tsx
@@ -1,15 +1,6 @@
 import { auth } from "@/auth";
 import ChangePasswordForm from "@/components/forms/change-password/ChangePasswordForm";
-import prisma from "@/lib/db";
 import { redirect } from "next/navigation";
-
-function getUser(id: string) {
-  const user = prisma.user.findUnique({
-    where: { id },
-    select: { password: true },
-  });
-  return user;
-}
 
 export default async function ChangePasswordPage() {
   const session = await auth();
@@ -18,16 +9,10 @@ export default async function ChangePasswordPage() {
     redirect("/login");
   }
 
-  const currentUser = await getUser(session.user.id);
-
-  if (!currentUser) {
-    redirect("/login");
-  }
-
   return (
     <div>
       <h1 className="text-4xl font-extrabold text-primary mb-6">Change Password</h1>
-      <ChangePasswordForm user={currentUser} />
+      <ChangePasswordForm />
     </div>
   );
 }
