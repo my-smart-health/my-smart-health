5737c6d3df8638e0c32f36db160d7afb654819ab
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-10-27 13:25:40 +0200

    refactor: update BioSection to use SeeMoreLess component for bio rendering

--- components/profile/profile-full/_components/BioSection.tsx
+++ components/profile/profile-full/_components/BioSection.tsx
@@ -3,12 +3,18 @@ import SeeMoreLess from "@/components/buttons/see-more-less/SeeMoreLess";
 
 export default function BioSection({ bio }: { bio: string }) {
   if (!bio) return null;
+
   return (
     <>
       <Divider addClass="my-4" />
       <section>
         <article className="text-base">
-          <SeeMoreLess text={bio} />
+          <SeeMoreLess>
+            <div
+              className="prose prose-sm max-w-none bio-content"
+              dangerouslySetInnerHTML={{ __html: bio }}
+            />
+          </SeeMoreLess>
         </article>
       </section>
     </>
