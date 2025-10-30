b740df3db004dd6c063ac924b7fb1ac9cbe8206a
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-10-30 15:40:14 +0200

    refactor: increase font size of expertise badges for better visibility

--- components/profile/profile-full/_components/FieldOfExpertiseSection.tsx
+++ components/profile/profile-full/_components/FieldOfExpertiseSection.tsx
@@ -76,9 +76,9 @@ export default function FieldOfExpertiseSection({ fieldOfExpertise }: FieldOfExp
 
   return (
     <>
-      <section className="flex flex-wrap gap-2 ml-2 my-auto">
+      <section className="flex flex-wrap gap-2 ml-2 my-auto ">
         {fieldOfExpertise.map((expertise) => (
-          <span key={expertise.id} className="badge p-3 text-sm font-semibold">
+          <span key={expertise.id} className="badge p-3 text-lg font-semibold">
             <TooltipItem expertise={expertise} />
           </span>
         ))}
