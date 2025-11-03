eb416ce08b072b8a33229f0aaf92d8e66fbc0d5a
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-11-03 18:51:12 +0200

    refactor: enhance NewsCarouselSkeleton with navigation and pagination features for improved user experience

--- components/carousels/newsCarousel/NewsCarouselSkeleton.tsx
+++ components/carousels/newsCarousel/NewsCarouselSkeleton.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, EffectCube, Mousewheel, Scrollbar } from "swiper/modules";
+import { Autoplay, EffectCube, Mousewheel, Navigation, Pagination, Scrollbar } from "swiper/modules";
+import { PAGINATION_BULLET_QUANTITY } from "@/utils/constants";
 
 type NewsCarouselSkeletonProps = {
   times?: number;
@@ -12,24 +13,28 @@ export default function NewsCarouselSkeleton({ times = 7 }: NewsCarouselSkeleton
   return (
     <div className="mx-auto max-w-2xs md:max-w-sm">
       <Swiper
-        modules={[Scrollbar, Mousewheel, Autoplay, EffectCube]}
+        modules={[Scrollbar, Navigation, Pagination, Mousewheel, Autoplay, EffectCube]}
         spaceBetween={0}
         slidesPerView={1}
         effect="cube"
+        grabCursor={true}
+        navigation={true}
+        pagination={{ clickable: true, enabled: true, dynamicBullets: true, dynamicMainBullets: PAGINATION_BULLET_QUANTITY }}
         cubeEffect={{
           shadow: true,
           slideShadows: true,
           shadowOffset: 20,
           shadowScale: 0.94
         }}
+
         mousewheel={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        autoplay={{ delay: 3000, disableOnInteraction: true, pauseOnMouseEnter: true, waitForTransition: true }}
         speed={300}
-        scrollbar={{ draggable: true }}
       >
         {Array.from({ length: times }).map((_, index) => (
           <SwiperSlide key={index}>
-            <div className="skeleton min-h-fit min-w-fit aspect-square">_</div>
+            <div className="skeleton min-h-fit min-w-fit aspect-square"></div>
+            <br />
             <br />
           </SwiperSlide>
         ))}
