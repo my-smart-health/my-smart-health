db41bd8d37a082e073209db611b0a886c1ba04df
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-11-05 16:46:56 +0200

    refactor: simplify JSX structure in PrescriptionReservation component

--- components/profile/profile-full/_components/PrescriptionReservation.tsx
+++ components/profile/profile-full/_components/PrescriptionReservation.tsx
@@ -13,43 +13,41 @@ export default function PrescriptionReservation({ reservationLinks }: Props) {
   if (links.length === 0) return null;
   return (
     <>
-      <>
-        <Divider addClass="my-4" />
-        <section className="flex flex-col items-center space-y-4">
-          <div className="flex flex-col gap-3 w-full mb-2">
-            {links.map((item, idx) => {
-              type LucideIcon = typeof CalendarPlus2;
-              let Icon: LucideIcon = CalendarPlus2;
-              let label = "online Termine";
-              switch (item.type) {
-                case RESERVATION_LINK_TYPES.OnlineTermine:
-                  Icon = CalendarPlus2;
-                  label = "online Termine";
-                  break;
-                case RESERVATION_LINK_TYPES.OnlineReservierungen:
-                  Icon = NotebookTabs;
-                  label = "online Reservierung";
-                  break;
-                case RESERVATION_LINK_TYPES.Rezeptbestellung:
-                  Icon = ClipboardPlus;
-                  label = "online Rezept";
-                  break;
-                case RESERVATION_LINK_TYPES.OnlineShop:
-                  Icon = ShoppingCart;
-                  label = "Online Shop";
-                  break;
-              }
-              return (
-                <div key={item.id ?? `${item.type}-${item.url}-${idx}`} className="flex align-middle w-full">
-                  <Link href={item.url} target="_self" className="btn btn-primary text-lg mx-auto flex gap-2 rounded">
-                    <Icon /> <span>{label}</span>
-                  </Link>
-                </div>
-              );
-            })}
-          </div>
-        </section>
-      </>
+      <Divider addClass="my-4" />
+      <section className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col gap-3 w-full mb-2">
+          {links.map((item, idx) => {
+            type LucideIcon = typeof CalendarPlus2;
+            let Icon: LucideIcon = CalendarPlus2;
+            let label = "online Termine";
+            switch (item.type) {
+              case RESERVATION_LINK_TYPES.OnlineTermine:
+                Icon = CalendarPlus2;
+                label = "online Termine";
+                break;
+              case RESERVATION_LINK_TYPES.OnlineReservierungen:
+                Icon = NotebookTabs;
+                label = "online Reservierung";
+                break;
+              case RESERVATION_LINK_TYPES.Rezeptbestellung:
+                Icon = ClipboardPlus;
+                label = "online Rezept";
+                break;
+              case RESERVATION_LINK_TYPES.OnlineShop:
+                Icon = ShoppingCart;
+                label = "Online Shop";
+                break;
+            }
+            return (
+              <div key={item.id ?? `${item.type}-${item.url}-${idx}`} className="flex align-middle w-full">
+                <Link href={item.url} target="_self" className="btn btn-primary text-lg mx-auto flex gap-2 rounded">
+                  <Icon /> <span>{label}</span>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </>
   );
 }
