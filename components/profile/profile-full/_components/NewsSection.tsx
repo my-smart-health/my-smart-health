5498ece989deec6834edb54818ee826c910e2548
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-11-07 13:11:34 +0200

    feat: adjust NewsSection layout by modifying heading margin and section padding for improved design consistency

--- components/profile/profile-full/_components/NewsSection.tsx
+++ components/profile/profile-full/_components/NewsSection.tsx
@@ -9,8 +9,8 @@ export default function NewsSection({ posts }: { posts: ProfileNewsCarouselItem[
   return (
     <>
       <Divider addClass="my-4" />
-      <h2 className="font-bold text-primary text-xl mb-2">News</h2>
-      <section className="w-full border border-primary rounded-lg p-4 bg-white/90 shadow-sm">
+      <h2 className="font-bold text-primary text-xl">News</h2>
+      <section className="w-full h-fit border border-primary rounded-lg p-2 shadow-sm">
         <ProfileNewsCarousel carouselItems={posts} />
       </section>
     </>
