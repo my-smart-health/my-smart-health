a1f93aa703cc132ea6afdeba46e47cd03e0ff4b6
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-11-26 14:44:48 +0200

    refactor: Define GoToButtonProps type for improved type safety and readability

--- components/buttons/go-to/GoToButton.tsx
+++ components/buttons/go-to/GoToButton.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 
-export default function GoToButton({ src, name, className = "text-blue-600 hover:underline", target = "_self" }: { src: string; name: string; className?: string; target?: string }) {
+type GoToButtonProps = {
+  src: string;
+  name: string;
+  className?: string;
+  target?: string;
+};
+
+export default function GoToButton({ src, name, className = "text-blue-600 hover:underline", target = "_self" }: GoToButtonProps) {
   return (
     <Link href={src} className={className} target={target}>
       {name}
