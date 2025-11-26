9d43e281817712a26015fd7a9b8a9d3c22296bd9
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-11-26 19:59:13 +0200

    fix: Enhance logout functionality by clearing session and local storage before signing out

--- components/buttons/log-out/LogOut.tsx
+++ components/buttons/log-out/LogOut.tsx
@@ -4,7 +4,15 @@ import { signOut } from "next-auth/react";
 
 export default function LogOut({ addClasses }: { addClasses?: string }) {
   const handleLogout = async () => {
-    await signOut({ callbackUrl: '/' });
+    if (typeof window !== 'undefined') {
+      sessionStorage.clear();
+      localStorage.removeItem('nextauth.message');
+    }
+
+    await signOut({
+      callbackUrl: '/',
+      redirect: true
+    });
   };
 
   return (
