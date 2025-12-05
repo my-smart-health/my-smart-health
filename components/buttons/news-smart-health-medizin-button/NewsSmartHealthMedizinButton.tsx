674068e78d3bab25bc0505a2f1addc6741f1c4be
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2025-12-05 18:36:35 +0200

    fix: Update layout of NewsSmartHealthMedizinButton for improved structure

--- components/buttons/news-smart-health-medizin-button/NewsSmartHealthMedizinButton.tsx
+++ components/buttons/news-smart-health-medizin-button/NewsSmartHealthMedizinButton.tsx
@@ -14,8 +14,17 @@ export default function NewsSmartHealthMedizinButton({ name, icon, goTo, active,
   return (
     <Link href={goTo} className={`flex flex-row ${imageAsTitle ? "" : "h-15"} w-full max-w-[100%] rounded-2xl border shadow-xl`}>
       {imageAsTitle ? (
-        <div className={`flex items-center justify-center w-full ${active ? 'bg-secondary rounded-2xl' : ''}`}>
+        <div className={`flex flex-col items-center  justify-center w-full ${active ? 'bg-secondary rounded-2xl' : ''}`}>
           <Image priority loading="eager" src={imageAsTitle} width={150} height={150} alt={`${name} icon`} style={{ objectFit: "contain", width: "auto", height: "auto" }} className={`w-auto h-auto mx-auto py-4 ${active ? 'bg-secondary rounded-2xl' : ''}`} />
+          <div className={`w-full mx-auto border h-0 max-w-[90%] `}></div>
+          <Image
+            src="/termine-kurzfristig-neutral.png"
+            alt="Termine Kurzfristig Icon"
+            width={65}
+            height={65}
+            style={{ width: "auto", height: "auto" }}
+            className="inline-block my-4"
+          />
         </div>
       ) : (
         <>
