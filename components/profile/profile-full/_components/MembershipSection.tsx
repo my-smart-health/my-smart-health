84c3081d7321d022bf4d7be2d60d31fa8bb7d0a8
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2026-01-06 13:45:37 +0200

    feat: remove unused Divider import from MembershipSection for cleaner code

--- components/profile/profile-full/_components/MembershipSection.tsx
+++ components/profile/profile-full/_components/MembershipSection.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Membership } from "@/utils/types";
-import Divider from "@/components/divider/Divider";
 
 type MembershipSectionProps = {
   membership: Membership;
