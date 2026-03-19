fb7ee856c1cfdcc6893ad7ebcc695c72e81a4f45
Radoslav Marinov <radoslav.a.marinov@gmail.com>
2026-03-19 21:31:56 +0200

    feat: reposition ProfileSearchToggle component

--- app/my-smart-health-termine-kurzfristig/layout.tsx
+++ app/my-smart-health-termine-kurzfristig/layout.tsx
@@ -10,12 +10,12 @@ export default async function SmartHealthLayout({ children }: { children: React.
   return (
     <>
       <MySmartHealth />
-      <ProfileSearchToggle className="max-w-5xl w-full" />
       <CategoryButton name={CATEGORY_NAMES.theLeadingDoctors.name} goTo={CATEGORY_NAMES.theLeadingDoctors.link} imageAsTitle="/the-leading-doctors.png" />
       <CategoryButton name={CATEGORY_NAMES.mySmartHealthTermineKurzfristig.name} goTo='/' imageAsTitle="/the-leading-hospitals.png" active />
       <div className="min-h-full w-full max-w-5xl flex flex-col gap-4">
         {children}
       </div>
+      <ProfileSearchToggle className="max-w-5xl w-full" />
       <CategoryButton name={CATEGORY_NAMES.smartHealth.name} icon="/icon3.png" goTo={CATEGORY_NAMES.smartHealth.link} />
       <CategoryButton name={CATEGORY_NAMES.medizinUndPflege.name} icon="/icon4.png" goTo={CATEGORY_NAMES.medizinUndPflege.link} />
       <CategoryButton name={CATEGORY_NAMES.notfalle.name} icon={<CirclePlus size={34} color="red" />} goTo={CATEGORY_NAMES.notfalle.link} />
--- app/the-leading-doctors/layout.tsx
+++ app/the-leading-doctors/layout.tsx
@@ -10,12 +10,12 @@ export default async function TheLeadingDoctorsLayout({ children }: { children: 
   return (
     <>
       <MySmartHealth />
-      <ProfileSearchToggle className="max-w-5xl w-full" />
       <CategoryButton name={CATEGORY_NAMES.theLeadingDoctors.name} icon="/icon2.png" goTo='/' active imageAsTitle="/the-leading-doctors.png" />
       <div className="min-h-full w-full max-w-5xl flex flex-col gap-4">
         {children}
       </div>
       <CategoryButton name={CATEGORY_NAMES.mySmartHealthTermineKurzfristig.name} goTo={CATEGORY_NAMES.mySmartHealthTermineKurzfristig.link} imageAsTitle="/the-leading-hospitals.png" />
+      <ProfileSearchToggle className="max-w-5xl w-full" />
       <CategoryButton name={CATEGORY_NAMES.smartHealth.name} icon="/icon3.png" goTo={CATEGORY_NAMES.smartHealth.link} />
       <CategoryButton name={CATEGORY_NAMES.medizinUndPflege.name} icon="/icon4.png" goTo={CATEGORY_NAMES.medizinUndPflege.link} />
       <CategoryButton name={CATEGORY_NAMES.notfalle.name} icon={<CirclePlus size={34} color="red" />} goTo={CATEGORY_NAMES.notfalle.link} />
